129fb4bf16ccf4907383b700a1116cdfef654df3
James Moynihan <jrmoynihan@gmail.com>
2022-08-06 11:17:45 -0400

    chore: :package: Added new exports
    Avoids long namespacing when importing the components or scripts

---
+++ src/lib/tooltip/index.ts
@@ -0,0 +1,4 @@
+import { tooltip, type TooltipDirections, type TooltipParameters } from './tooltip-action';
+import ActionTooltip from './ActionTooltip.svelte';
+export { ActionTooltip };
+export { tooltip, type TooltipDirections, type TooltipParameters };
