d291306edd13235af41fe3873dce5c0160d17fb7
James Moynihan <jrmoynihan@gmail.com>
2022-08-07 08:40:42 -0400

    docs: :alembic: Provided an example of the tooltip usage in the routes.
    Going to start fleshing out how I showcase each contribution to the library by giving page/route examples.

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,13 @@
-<h1>Welcome to SvelteKit</h1>
+<script lang="ts">
+	import { tooltip } from '$lib/tooltip/tooltip-action';
+</script>
+
+<h1 use:tooltip={{ position: 'left' }}>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<style lang="scss">
+	h1 {
+		margin: auto;
+		max-width: max-content;
+	}
+</style>
