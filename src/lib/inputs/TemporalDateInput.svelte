0736d8e3bd0b7c32799b34834f73bf1b9198b60a
James Moynihan <jrmoynihan@gmail.com>
2022-08-19 03:06:16 -0400

    feat(Inputs): :sparkles: Use custom tooltip instead of block-level element to show validation message.
    Rather than letting the DateInput unpredictably shift around its neighboring elements, the tooltip gives a highly visible error notification, but without shifting content.

--- src/lib/inputs/TemporalDateInput.svelte
+++ src/lib/inputs/TemporalDateInput.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+	import { tooltip, type TooltipDirections } from '$lib/tooltip';
+
 	import { Temporal } from '@js-temporal/polyfill';
 	import { createEventDispatcher } from 'svelte';
 	import { fly } from 'svelte/transition';
@@ -13,7 +15,15 @@
 	export let label_text: string = '';
 	export let label_styles: string = '';
 	export let input_styles: string = '';
-	export let invalid_msg: string | undefined = '';
+	export let invalid_msg: string | null | undefined = null;
+	export let invalid_msg_position: TooltipDirections = 'top';
+	export let invalid_tooltip_css: [string, string][] = [
+		['font-size', 'smaller'],
+		['color', 'var(--date-input-invalid-message-color, salmon)'],
+		['background', 'var(--date-input-invalid-message-background, white)'],
+		['padding', '0.5rem'],
+		['border-radius', '1rem']
+	];
 	export let label_position: 'before' | 'after' = 'before';
 	export let use_transition: boolean = false;
 	export let transition = fly;
@@ -47,23 +57,32 @@
 				return `${date_string}${time_string}`;
 			case 'time':
 				return time_string;
+			default:
+				return date_string;
 		}
 	};
 	const stringToTemporalDate = (date_string: string) => {
 		const yyyy_mm_dd = date_string.split('T')[0];
-		const hh_mm_ss = date_string.split('T')[1];
-		const [year, month, day] = yyyy_mm_dd.split('-');
-		const [hour, minute, second] = hh_mm_ss.split(':');
+		const [year, month, day] = yyyy_mm_dd?.split('-');
 
 		const new_date = date.with({
 			year: parseInt(year),
 			month: parseInt(month),
-			day: parseInt(day),
-			hour: parseInt(hour),
-			minute: parseInt(minute),
-			second: parseInt(second)
+			day: parseInt(day)
 		});
-		return new_date;
+		switch (type) {
+			case 'date':
+				return new_date;
+			default:
+				const hh_mm_ss = date_string.split('T')[1];
+				const [hour, minute, second] = hh_mm_ss?.split(':');
+				const new_date_with_time = new_date.with({
+					hour: parseInt(hour),
+					minute: parseInt(minute),
+					second: parseInt(second)
+				});
+				return new_date_with_time;
+		}
 	};
 	let internal_string_date: string = temporalDateToString(date);
 	let max_internal: string = temporalDateToString(max);
@@ -74,21 +93,27 @@
 	$: min_internal = temporalDateToString(min);
 	$: {
 		date;
-		is_valid = dateInput?.checkValidity();
-		invalid_msg = dateInput?.validationMessage;
+		is_valid = dateInput?.checkValidity() ?? true;
+		invalid_msg = invalid_msg ? invalid_msg : dateInput?.validationMessage;
 	}
-	$: console.log('internal', internal_string_date);
-	$: console.log('date', date);
+	// $: console.log('internal', internal_string_date);
+	// $: console.log('date', date);
 </script>
 
-<label for={name} style={label_styles} transition:transition={transition_params}>
+<label
+	for={name}
+	style={label_styles}
+	transition:transition={transition_params}
+	use:tooltip={{
+		position: invalid_msg_position,
+		title: `${invalid_msg}`,
+		visible: !is_valid,
+		disabled: is_valid || !invalid_msg,
+		css: invalid_tooltip_css
+	}}
+>
 	{#if label_position === 'before'}
 		{label_text}
-		{#if !is_valid}
-			<div class="invalid-msg">
-				{invalid_msg}
-			</div>
-		{/if}
 		<slot name="label-text" />
 	{/if}
 	<!-- NOTE: Can't use dynamic {type} with bind:value, so instead we'll conditionally create different types of date inputs -->
@@ -171,13 +196,5 @@
 		&:invalid {
 			border: var(--date-input-invalid-border, 3px solid salmon);
 		}
-	}
-	.invalid-msg {
-		font-size: smaller;
-		font-style: italic;
-		color: var(--date-input-invalid-message-color, salmon);
-		background: var(--date-input-invalid-message-background, inherit);
-		padding: 0.25rem 0.5rem;
-		border-radius: 3vh;
 	}
 </style>
