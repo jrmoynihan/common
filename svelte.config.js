e594f5d9949efd5ccc28af59c125f612a11606d1
James Moynihan <jrmoynihan@gmail.com>
2022-08-27 22:29:12 -0400

    mdsvex, experimental config features

--- svelte.config.js
+++ svelte.config.js
@@ -1,16 +1,29 @@
+import { mdsvex } from 'mdsvex';
+import mdsvexConfig from './mdsvex.config.js';
 import adapter from '@sveltejs/adapter-auto';
 import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+	extensions: ['.svelte', ...mdsvexConfig.extensions],
+
 	// Consult https://github.com/sveltejs/svelte-preprocess
 	// for more information about preprocessors
-	preprocess: preprocess({
-		scss: true
-	}),
+	preprocess: [
+		preprocess({
+			scss: true
+		}),
+		mdsvex(mdsvexConfig)
+	],
 
 	kit: {
 		adapter: adapter()
+	},
+
+	vitePlugin: {
+		experimental: {
+			inspector: true
+		}
 	}
 };
 
