e3519b0b1b3badc826852bc3ecb1a1ae6b78846e
James Moynihan <jrmoynihan@gmail.com>
2022-08-28 00:47:52 -0400

    try $app alias fix

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,14 @@
 import { sveltekit } from '@sveltejs/kit/vite';
+import path from 'path';
 
 /** @type {import('vite').UserConfig} */
 const config = {
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	resolve: {
+		alias: {
+			$app: path.resolve('.svelte-kit/runtime/app')
+		}
+	}
 };
 
 export default config;
