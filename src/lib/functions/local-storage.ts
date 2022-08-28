3f8e5c5e97df68ed550d7949840abcec89c70fe0
James Moynihan <jrmoynihan@gmail.com>
2022-08-27 22:38:10 -0400

    add get/set local storage functions

---
+++ src/lib/functions/local-storage.ts
@@ -0,0 +1,21 @@
+import { browser } from '$app/environment';
+import { myLog } from './logging';
+
+export const getLocalStorageItem = async <T>(key: string): Promise<any> => {
+	if (browser) {
+		const item = localStorage.getItem(key);
+		if (item) {
+			const parsed_item = JSON.parse(item);
+			return parsed_item ?? null;
+		}
+	} else {
+		myLog({ msg: 'unable to check for local storage without the browser available' });
+	}
+};
+export const setLocalStorageItem = async (key: string, value: unknown): Promise<void> => {
+	if (browser) {
+		localStorage.setItem(key, JSON.stringify(value));
+	} else {
+		myLog({ msg: 'unable to set item in local storage without the browser available' });
+	}
+};
