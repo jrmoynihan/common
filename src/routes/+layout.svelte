f968f81940dd023ee9ba504462407dcb1f9a10d7
James Moynihan <jrmoynihan@gmail.com>
2022-08-27 23:04:13 -0400

    refactor: :art: Add nav tooltip intros and exits, light/dark mode toggle, nav. Use new Transition component's transition interface.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,21 +1,51 @@
 <script lang="ts">
-	import { dev } from '$app/env';
+	import { dev } from '$app/environment';
 	import { beforeNavigate } from '$app/navigation';
-	import { page } from '$app/stores';
+	import type { TooltipParameters } from '$lib';
+	import LightDarkToggle from '$lib/buttons/LightDarkToggle.svelte';
+	import Navigation from '$lib/navigation/Navigation.svelte';
 	import Transition from '$lib/wrappers/Transition.svelte';
 	import { writable } from 'svelte/store';
+	import { fly } from 'svelte/transition';
+	import { onDestroy } from 'svelte';
+	import { use_dark_theme } from './stores';
+	import '../../src/mdsvex.css';
 
 	let coords = writable({ x: 0, y: 0 });
-	let links = ['tooltips', 'inputs', 'buttons', 'wrappers'];
+	let links = ['tooltips', 'inputs', 'buttons', 'wrappers', 'recipes'];
 	let refresh: boolean = false;
+	let tooltips_visible = true;
+	let tooltips_disabled = false;
 
-	// Determine if the current path matches a given path string
-	function doesPathMatchCurrentURL(path: string, currentURL: string) {
-		return currentURL.includes(`/${path}`);
-	}
-	function capitalize(str: string) {
-		return str.charAt(0).toUpperCase() + str.slice(1);
-	}
+	const delay_tips = (i: number) => {
+		return (i + 1) * 100 + 100;
+	};
+	let tooltip_words = ['Click', 'on a link', 'to explore', 'the cool stuff', 'available!'];
+	const getTitleSegment = (i: number) => {
+		return tooltip_words[i];
+	};
+	const assignTooltips = () =>
+		tooltip_words.map((word, i) => {
+			const delay = delay_tips(i);
+			const title = getTitleSegment(i);
+			return {
+				visible: tooltips_visible,
+				disabled: tooltips_disabled,
+				delay,
+				title
+			};
+		});
+	let link_tooltip_options: TooltipParameters[] = assignTooltips();
+
+	const disableTooltips = () => {
+		const new_options = link_tooltip_options.map((tooltip) => {
+			const { ...existing_options } = tooltip;
+			return { ...existing_options, visible: false, disabled: true };
+		});
+		// Trigger reactive update
+		link_tooltip_options = [...new_options];
+	};
+
 	beforeNavigate((nav) => {
 		const { from, to } = nav;
 		if (from?.pathname === to?.pathname) return;
@@ -26,29 +56,37 @@
 				refresh = !refresh;
 			}
 		}
+		disableTooltips();
 	});
+	const countdown = setTimeout(() => {
+		disableTooltips();
+		console.log('ran countdown');
+	}, 5_000);
+
+	onDestroy(() => clearTimeout(countdown));
 </script>
 
 <svelte:window on:mousemove={(e) => coords.set({ x: e.clientX, y: e.clientY })} />
 {#if dev}
-	<div class="top-left" style="color:orange">
+	<div class="top left" class:dark={$use_dark_theme} style="color:var(--accent,orange)">
 		<p>X: {$coords.x}</p>
 		<p>Y: {$coords.y}</p>
 	</div>
 {/if}
+<div class="top right">
+	<LightDarkToggle bind:use_dark_theme={$use_dark_theme} />
+</div>
 <h1>
 	<a href="/" sveltekit:prefetch class="cool-text">The Commons</a>
 </h1>
-<section>
-	{#each links as link}
-		{@const is_current_page = doesPathMatchCurrentURL(link, $page.url.pathname)}
-		<a href="/{link}" sveltekit:prefetch class="link" class:current-page={is_current_page}
-			>{capitalize(link)}</a
-		>
-	{/each}
-</section>
+<Navigation {links} bind:tooltip_options={link_tooltip_options} />
 <main>
-	<Transition bind:refresh>
+	<Transition
+		bind:refresh
+		transition={fly}
+		in_transition_parameters={{ duration: 400, x: -100, delay: 450 }}
+		out_transition_parameters={{ duration: 400, x: 100 }}
+	>
 		<slot />
 	</Transition>
 </main>
@@ -57,17 +95,22 @@
 	:root {
 		font-family: Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', -apple-system,
 			BlinkMacSystemFont, 'Segoe UI', sans-serif;
-		background-color: hsl(195, 61%, 14%);
-		color: white;
+		background-color: var(--background, hsl(195, 61%, 14%));
+		color: var(--text, white);
 		--tooltip-color: black;
 		--tooltip-font-weight: 400;
 		--tooltip-font-size: 12px;
 	}
-	.top-left {
+	.top {
 		position: fixed;
 		top: 0;
-		left: 0;
 		padding: 1rem 2rem;
+	}
+	.left {
+		left: 0;
+	}
+	.right {
+		right: 0;
 	}
 	h1 {
 		margin: 2rem auto;
@@ -85,7 +128,7 @@
 	}
 	.cool-text {
 		display: inline-flex;
-		color: orange;
+		color: var(--accent, orange);
 		text-decoration: underline;
 		transition: background-color 0.2s ease-in-out;
 		padding: 0.5rem;
@@ -93,31 +136,6 @@
 		&:hover,
 		&:focus-visible {
 			background-color: hsl(195, 61%, 34%);
-		}
-	}
-	section {
-		display: flex;
-		align-items: center;
-		justify-content: center;
-		margin: 2rem auto;
-		max-width: max-content;
-		gap: 1rem;
-	}
-	.link {
-		text-decoration: none;
-		font-size: 1.5rem;
-		padding: 1rem;
-		color: white;
-		border-radius: 2rem;
-		background-color: hsl(195, 61%, 34%);
-		transition: background-color 0.2s ease-in-out, box-shadow 0.2s ease-in-out;
-		&:hover,
-		&:focus-visible {
-			background-color: hsl(195, 61%, 44%);
-		}
-		&.current-page {
-			box-shadow: 0 0 14px 4px orange;
-			background-color: hsl(195, 61%, 44%);
 		}
 	}
 </style>
