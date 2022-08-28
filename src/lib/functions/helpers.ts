2da726f2a771e71b8e2e9be72dbdc3c622cca21e
James Moynihan <jrmoynihan@gmail.com>
2022-08-27 22:37:50 -0400

    add capitalize(), arrayFromNumber(), delay() fn's

--- src/lib/functions/helpers.ts
+++ src/lib/functions/helpers.ts
@@ -1,10 +1,15 @@
+export function capitalize(str: string) {
+	return str.charAt(0).toUpperCase() + str.slice(1);
+}
 // A function to make an array from a number
-export function arrayFromNumber(n: number) {
-	return Array(n)
-		.fill(0)
-		.map((_, i) => i);
+export function arrayFromNumber(n: number, map_function?: (a: any, i: number) => any) {
+	if (!map_function) map_function = (_: any, i?: number) => i;
+	return Array(n).fill(0).map(map_function);
 }
 // A function to clamp a number between two numbers
 export function clamp(n: number, min: number, max: number) {
 	return Math.min(Math.max(n, min), max);
+}
+export async function delay(time: number) {
+	await new Promise((args) => setTimeout(args, time));
 }
