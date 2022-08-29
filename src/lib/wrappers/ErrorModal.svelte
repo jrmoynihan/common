ba595c5cb75050b6bfa4825a5898f45e75919d5f
James Moynihan <jrmoynihan@gmail.com>
2022-08-29 14:08:26 -0400

    addd ErrorModal component

---
+++ src/lib/wrappers/ErrorModal.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import Modal from './Modal.svelte';
+
+	export let error = '';
+</script>
+
+<Modal dialog_starts_open={true} is_error={true}>
+	<slot slot="modal-content" />
+	{error}
+</Modal>
--- src/lib/wrappers/index.ts
+++ src/lib/wrappers/index.ts
@@ -1,8 +1,9 @@
 import Accordion from './Accordion.svelte';
 import Modal from './Modal.svelte';
+import ErrorModal from './ErrorModal.svelte';
 import ModalWithButton from './ModalWithButton.svelte';
 import Tabs from './Tabs.svelte';
 import Transition from './Transition.svelte';
 import { Tab } from './tab';
 
-export { Accordion, Modal, ModalWithButton, Tabs, Transition, Tab };
+export { Accordion, Modal, ErrorModal, ModalWithButton, Tabs, Transition, Tab };
