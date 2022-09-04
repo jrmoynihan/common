645873d0f478564c05db7040fc4ec3b06c37e865
James Moynihan <jrmoynihan@gmail.com>
2022-09-04 00:09:45 -0400

    tweak default markdown styles for code blocks

--- mdsvex.config.js
+++ mdsvex.config.js
@@ -1,11 +1,12 @@
 import { defineMDSveXConfig as defineConfig } from 'mdsvex';
+import codesandbox from 'remark-codesandbox';
 
 const config = defineConfig({
 	extensions: ['.svelte.md', '.md', '.svx'],
 
 	smartypants: true,
 
-	remarkPlugins: [],
+	remarkPlugins: [codesandbox({ mode: 'button' })],
 	rehypePlugins: []
 });
 
