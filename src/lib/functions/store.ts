d2b9081ae168e6b707775616569ec7bc7c386c90
James Moynihan <jrmoynihan@gmail.com>
2022-09-05 10:19:12 -0400

    feat(Functions): :sparkles: Add setDerivedContext and getDerivedContext functions. Add start/stop notifiers for readable and writable context functions.
    derived context allows use of derived store patterns inside context variable lookups.  Start/stop notifiers allow granular control of subscriptions to a store's changes.

--- src/lib/functions/store.ts
+++ src/lib/functions/store.ts
@@ -1,19 +1,50 @@
 import { getContext, setContext } from 'svelte';
-import { readable, writable, type Readable, type Writable } from 'svelte/store';
+import {
+	derived,
+	readable,
+	writable,
+	type Readable,
+	type Writable,
+	type Stores,
+	type StoresValues,
+	get,
+	type StartStopNotifier,
+	type Unsubscriber
+} from 'svelte/store';
 
-export function setWritableContext<T>(key: string, value: T): Writable<T> {
+export function setWritableContext<T>(
+	key: string,
+	value: T,
+	start?: StartStopNotifier<T>
+): Writable<T> {
 	const unique_key = Symbol.for(key);
-	return setContext(unique_key, writable<T>(value));
+	return setContext(unique_key, writable<T>(value, start));
 }
 export function getWritableContext<T>(key: string): Writable<T> {
 	const unique_key = Symbol.for(key);
 	return getContext<Writable<T>>(unique_key);
 }
-export function setReadableContext<T>(key: string, value: T): Readable<T> {
+export function setReadableContext<T>(
+	key: string,
+	value: T,
+	start?: StartStopNotifier<T>
+): Readable<T> {
 	const unique_key = Symbol.for(key);
-	return setContext(unique_key, readable<T>(value));
+	return setContext(unique_key, readable<T>(value, start));
 }
 export function getReadableContext<T>(key: string): Readable<T> {
 	const unique_key = Symbol.for(key);
 	return getContext<Readable<T>>(unique_key);
+}
+export function setDerivedContext<S, T>(
+	key: string,
+	stores: S extends Stores ? any : any,
+	callback: (values: StoresValues<S>, set: (value: T) => void) => Unsubscriber | void,
+	initial_value?: T
+): Readable<T> {
+	const unique_key = Symbol.for(key);
+	return setContext(unique_key, derived(stores, callback, initial_value));
+}
+export function getDerivedContext<T>(key: string): Readable<T> {
+	return getReadableContext(key);
 }
