f50dfb342801876b42f4e740bf054371d8a79bd9
James Moynihan <jrmoynihan@gmail.com>
2022-09-05 13:30:02 -0400

    feat: :technologist: Add Code and ExternalLink components
    Make internal styles more consistent and easy-to-use across pages.

---
+++ src/lib/internal/Code.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	import { use_dark_theme } from '../../routes/stores';
+</script>
+
+<code class="my-code" class:dark={$use_dark_theme}>
+	<slot />
+</code>
+
+<style lang="scss">
+	:global(.my-code) {
+		background-color: hsla(195, 81%, 24%, 60%);
+		padding: 0 0.25rem;
+		border-radius: 0.25rem;
+		font-family: 'Fira Code iScript', 'Fira Code', Menlo, Monaco, Consolas, 'Andale Mono',
+			'Ubuntu Mono', 'Courier New', monospace;
+		line-height: 1.5em;
+		color: white;
+		&.dark {
+			background-color: hsla(195, 81%, 24%, 90%);
+		}
+	}
+</style>
---
+++ src/lib/internal/ExternalLink.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+	import { use_dark_theme } from '../../routes/stores';
+	export let url: URL;
+</script>
+
+<mark><a href={url.href} data-sveltekit-prefetch class:dark={$use_dark_theme}><slot /></a></mark>
+
+<style lang="scss">
+	:global(mark) {
+		background-color: transparent;
+		padding: 0 0.25rem;
+		border-radius: 0.25rem;
+		&:has(code) {
+			padding: 0;
+		}
+	}
+	:global(a) {
+		color: hsl(39, 100%, 50%);
+		&:visited {
+			color: hsl(15deg 100% 50%);
+			&.dark {
+				color: hsl(15deg 100% 70%);
+			}
+		}
+	}
+</style>
