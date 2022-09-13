79d7ce96003558c4907b568e6389862c11872044
James Moynihan <jrmoynihan@gmail.com>
2022-09-13 10:37:29 -0400

    update exports

--- src/lib/navigation/index.ts
+++ src/lib/navigation/index.ts
@@ -1,5 +1,14 @@
 import Navigation from './Navigation.svelte';
 import NavItem from './NavLink.svelte';
-import { type NavigationLink, makeNavLinks } from './navLink';
+import AnchorHeading from './AnchorHeading.svelte';
+import LinkableAnchorHeading from './LinkableAnchorHeading.svelte';
+export {
+	type NavigationLink,
+	type makeNavLinksOptions,
+	makeNavLinks,
+	makeAnchorLinks,
+	makeLinks,
+	shouldLayoutTransitionOnNavigation
+} from './nav-functions';
 
-export { Navigation, NavItem, NavigationLink, makeNavLinks };
+export { Navigation, NavItem, AnchorHeading, LinkableAnchorHeading };
