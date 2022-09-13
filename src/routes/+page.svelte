576267bbf1cf9669761e08c32efdce992dd5aff8
James Moynihan <jrmoynihan@gmail.com>
2022-09-13 14:11:53 -0400

    remove context function tinker tests

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,33 +1,4 @@
-<script lang="ts">
-	import {
-		getDerivedContext,
-		getReadableContext,
-		setDerivedContext,
-		setWritableContext
-	} from '$lib/functions/store';
-	import { writable, type Readable } from 'svelte/store';
-
-	const first = writable(1);
-	const second = setWritableContext('second', 10);
-	const storeArray = [first, second];
-	const product = setDerivedContext(
-		'product',
-		storeArray,
-		// What types of values will the derived store consume?  What method will it use
-		([a, b]: [number, number], set: (new_value: number) => void) => {
-			const new_number = a * b;
-			set(new_number);
-		}
-	);
-	// These are equivalent:
-	const found = getReadableContext<number>('product');
-	const foundAgain: Readable<number> = getDerivedContext<number>('product');
-</script>
-
 <p>Click on a link to see examples</p>
-<button on:click={() => $first++}>first number: {$first}</button>
-<button on:click={() => $second++}>second number: {$second}</button>
-<output>Product: {$product}</output>
 
 <style lang="scss">
 	p {
