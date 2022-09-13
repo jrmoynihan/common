b02080bf690835d9a8ac7292c8b128fc8d9eac67
James Moynihan <jrmoynihan@gmail.com>
2022-09-13 10:37:05 -0400

    feat(Navigation): :sparkles: Add icon props to nav links and add scrollIntoView function
    Allow icons to be displayed adjacent to nav links and allow anchor links to be scrolled into view smoothly

--- src/lib/navigation/NavLink.svelte
+++ src/lib/navigation/NavLink.svelte
@@ -2,13 +2,16 @@
 	import { page } from '$app/stores';
 	import { tooltip, type TooltipParameters } from '$lib/tooltip';
 	import { capitalize } from '$lib/functions';
+	import { Fa, FaLayers, FaLayersText } from 'svelte-fa/src';
+	import type { IconLayer } from './nav-functions';
+	import { goto } from '$app/navigation';
 
 	/** The URL object describing the link */
 	export let url: URL;
 	/** The displayed text for the link (defaults to the link's pathname)*/
 	export let link_text: string = capitalize(url.pathname);
 	/** Tooltip options to apply to the nav links. If an array is passed, each options object will be applied to each link, individually.  Must pass the index in the array, `i` as well!  */
-	export let tooltip_options: TooltipParameters | TooltipParameters[] = [];
+	export let tooltip_options: TooltipParameters | TooltipParameters[] = { disabled: true };
 	/** Is the link the currently active page/url? */
 	export let is_current_page: boolean = false;
 	/** Index used to access tooltip options from an array */
@@ -21,6 +24,9 @@
 	export let hover_styles: [string, string][] = [];
 	/** Focus styles to apply to the links. */
 	export let focus_styles: [string, string][] = [];
+	/** Pass in an array of icons to use in a FaLayer component. */
+	export let icons: IconLayer[] | null = null;
+
 	let anchor: HTMLAnchorElement;
 	let styles = static_styles;
 	let hovered = false;
@@ -30,7 +36,23 @@
 	function doesPathMatchCurrentURL(path: string, currentURL: string) {
 		return currentURL.includes(`${path}`);
 	}
-	$: is_current_page = doesPathMatchCurrentURL(url.pathname, $page?.url?.pathname);
+
+	function scrollIntoView(e: MouseEvent & { currentTarget: EventTarget & HTMLElement }): void {
+		//@ts-ignore
+		const href: string = e.target?.getAttribute('href');
+		if (href) {
+			const el = document?.getElementById(href.split('#')[1]);
+			if (el) {
+				el?.scrollIntoView({
+					behavior: 'smooth'
+				});
+			} else {
+				goto(href);
+			}
+		}
+	}
+
+	$: is_current_page = doesPathMatchCurrentURL(url.href, $page?.url?.href);
 	$: {
 		if (anchor && current_page_styles?.length > 0 && is_current_page) {
 			current_page_styles.forEach(([key, value]) => {
@@ -84,8 +106,23 @@
 	on:mouseleave={() => (hovered = false)}
 	on:focus={() => (focused = true)}
 	on:blur={() => (focused = false)}
-	on:click>{link_text}</a
+	on:click|preventDefault={scrollIntoView}
 >
+	{#if Array.isArray(icons) && icons.length > 0}
+		<FaLayers>
+			{#each icons as { icon, translateX, translateY, color, text, size, scale, style }}
+				{#if icon}
+					<Fa {icon} {size} {translateX} {translateY} {color} {scale} {style} />
+				{:else if text}
+					<FaLayersText {translateX} {translateY} {color} {scale} {style}>
+						{text}
+					</FaLayersText>
+				{/if}
+			{/each}
+		</FaLayers>
+	{/if}
+	{link_text}
+</a>
 
 <style lang="scss">
 	.link {
