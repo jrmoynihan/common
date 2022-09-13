614ba8cee1a0155a566cfd5be3e3bde676dc8898
James Moynihan <jrmoynihan@gmail.com>
2022-09-13 14:22:24 -0400

    type the data values returned

--- src/routes/recipes/gallery/+page.ts
+++ src/routes/recipes/gallery/+page.ts
@@ -19,14 +19,17 @@ export function load(event: LoadEvent) {
 	];
 	const double_seeds = [...seeds];
 	const number_of_links = double_seeds.length;
-	const images = arrayFromNumber(number_of_links, (_, i: number) => {
-		return {
-			path: `route-${i}`,
-			text: 'random',
-			href: `https://source.unsplash.com/random?${double_seeds[i]}`,
-			uuid: crypto.randomUUID()
-		};
-	});
+	const images: { path: string; text: string; href: string; uuid: string }[] = arrayFromNumber(
+		number_of_links,
+		(_, i: number) => {
+			return {
+				path: `route-${i}`,
+				text: 'random',
+				href: `https://source.unsplash.com/random?${double_seeds[i]}`,
+				uuid: crypto.randomUUID()
+			};
+		}
+	);
 	return { images };
 	throw error(404, 'Not found.');
 }
