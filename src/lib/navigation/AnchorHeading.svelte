05490a8ffb4611e28955c2804071c36c2212e963
James Moynihan <jrmoynihan@gmail.com>
2022-09-13 10:32:28 -0400

    feat(Navigation): :sparkles: Add AnchorHeading and LinkableAnchorHeading components
    Provides a scaffold for creating documentation anchors with buttons to copy links to the anchors directly.

---
+++ src/lib/navigation/AnchorHeading.svelte
@@ -0,0 +1,47 @@
+<script lang="ts">
+	export let id: string;
+	export let text: string;
+	export let header_type: 1 | 2 | 3 | 4 | 5 | 6 = 3;
+</script>
+
+<a {id}>
+	{#if header_type === 1}
+		<h1>
+			{text}
+			<slot />
+		</h1>
+	{/if}
+	{#if header_type === 2}
+		<h2>
+			{text}
+			<slot />
+		</h2>
+	{/if}
+	{#if header_type === 3}
+		<h3>
+			{text}
+			<slot />
+		</h3>
+	{/if}
+	{#if header_type === 4}
+		<h4>
+			{text}
+			<slot />
+		</h4>
+	{/if}
+	{#if header_type === 5}
+		<h5>
+			{text}
+			<slot />
+		</h5>
+	{/if}
+	{#if header_type === 6}
+		<h6>
+			{text}
+			<slot />
+		</h6>
+	{/if}
+</a>
+
+<style lang="scss">
+</style>
---
+++ src/lib/navigation/LinkableAnchorHeading.svelte
@@ -0,0 +1,52 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+	import Button from '$lib/buttons/Button.svelte';
+	import { defaultToast } from '$lib/toasts';
+	import { faLink } from '@fortawesome/free-solid-svg-icons/index';
+	import { onDestroy, type ComponentProps } from 'svelte';
+
+	import AnchorHeading from './AnchorHeading.svelte';
+
+	let copied = false;
+	let timeout: NodeJS.Timeout;
+	export let static_styles =
+		'border: 0; --button-opacity: 0.5; padding: 0rem 0.5rem; max-height: max-content; margin: 0.25rem 0.5rem;';
+	export let copied_styles = 'background: hsla(var(--link-background-value), 75%);';
+	export let button_placement: 'before' | 'after' = 'after';
+	export let heading_props: ComponentProps<AnchorHeading>;
+	export let button_props: ComponentProps<Button> = {
+		static_styles,
+		hover_styles: [['--button-hover-opacity', '1']]
+	};
+
+	function copyLinkAddress() {
+		const { id } = heading_props;
+		const link = `${$page.url.origin}/${$page.routeId}#${id}`;
+		navigator.clipboard.writeText(link);
+		copied = true;
+		defaultToast({ msg: 'Copied Link!', duration: 2_000 });
+		timeout = setTimeout(() => (copied = false), 20_000);
+	}
+
+	$: button_props.static_styles = copied ? static_styles.concat(copied_styles) : static_styles;
+
+	onDestroy(() => clearTimeout(timeout));
+</script>
+
+<div class="linkable-heading">
+	{#if button_placement === 'before'}
+		<Button icon={faLink} on:click={copyLinkAddress} {...button_props} />
+	{/if}
+	<AnchorHeading {...heading_props}>
+		<slot />
+	</AnchorHeading>
+	{#if button_placement === 'after'}
+		<Button icon={faLink} on:click={copyLinkAddress} {...button_props} />
+	{/if}
+</div>
+
+<style lang="scss">
+	.linkable-heading {
+		display: flex;
+	}
+</style>
