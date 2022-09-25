8d3fd23d586497a4fe983e7a6378fd1f699ec8a9
James Moynihan <jrmoynihan@gmail.com>
2022-09-24 20:53:09 -0400

    use relative imports for the types after pkg'ing

--- src/lib/index.d.ts
+++ src/lib/index.d.ts
@@ -1,7 +1,86 @@
-export * from './tooltip/index.js';
-export * from './inputs/index.js';
-export * from './buttons/index.js';
-export * from './functions/index.js';
-export * from './navigation/index.js';
-export * from './toasts/index.js';
-export * from './wrappers/index.js';
+//@ts-nocheck
+// Buttons
+import Button from './lib/buttons/Button.svelte';
+import LightDarkToggle from './lib/buttons/LightDarkToggle.svelte';
+import ToggleSwitch from './lib/buttons/ToggleSwitch.svelte';
+export { Button, LightDarkToggle, ToggleSwitch };
+export * from './lib/buttons';
+// Functions
+export {
+	capitalize,
+	arrayFromNumber,
+	delay,
+	deKebab,
+	clamp,
+	splitCamelCase,
+	splitSnakeCase,
+	removeSpecialCharacters,
+	getRandomBetween,
+	getMax,
+	getMin,
+	getMinDate,
+	getMaxDate,
+	flattenObjectRecursively,
+	getAncestors,
+	getTransitionDurations
+} from './lib/functions/helpers.js';
+export {
+	getLocalStorageItem,
+	setLocalStorageItem,
+	deleteLocalStorageItem
+} from './lib/functions/local-storage.js';
+export {
+	Log,
+	WarningLog,
+	ErrorLog,
+	LogAndToast,
+	ErrorAndToast,
+	all_icons,
+	defaultConsoleLogStyle
+} from './lib/functions/logging.js';
+export {
+	setWritableContext,
+	getWritableContext,
+	setReadableContext,
+	getReadableContext,
+	setDerivedContext,
+	getDerivedContext
+} from './lib/functions/store.js';
+import TextInput from './lib/inputs/TextInput.svelte';
+import TemporalDateInput from './lib/inputs/TemporalDateInput.svelte';
+import NumericInput from './lib/inputs/NumericInput.svelte';
+export { TextInput, TemporalDateInput, NumericInput };
+export * from './lib/functions';
+// Navigation
+import Navigation from './lib/navigation/Navigation.svelte';
+import NavLink from './lib/navigation/NavLink.svelte';
+import AnchorHeading from './lib/navigation/AnchorHeading.svelte';
+import LinkableAnchorHeading from './lib/navigation/LinkableAnchorHeading.svelte';
+import ScrollProgress from './lib/navigation/ScrollProgress.svelte';
+export {
+	makeNavLinks,
+	makeAnchorLinks,
+	makeLinks,
+	shouldLayoutTransitionOnNavigation
+} from './lib/navigation/nav-functions.js';
+export { Navigation, NavLink, AnchorHeading, LinkableAnchorHeading, ScrollProgress };
+// Toasts
+import SeenToast from './lib/toasts/SeenToast.svelte';
+export { SeenToast };
+export { defaultToast, errorToast, reminderToast } from './lib/toasts/toasts.js';
+// Tooltips
+export { tooltip } from './lib/tooltip/tooltip-action.js';
+import ActionTooltip from './lib/tooltip/ActionTooltip.svelte';
+export { ActionTooltip };
+// Wrappers
+export { Tab } from './lib/wrappers/tab.js';
+export {
+	Accordion,
+	Modal,
+	ErrorModal,
+	ModalWithButton,
+	Tabs,
+	Transition
+} from './lib/wrappers/index.js';
+
+export * from './lib/index';
