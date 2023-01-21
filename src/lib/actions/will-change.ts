e5afc00e9c7900a04b70ad1b65a5c07d525416cc
James Moynihan <jrmoynihan@gmail.com>
2023-01-21 11:35:33 -0500

    add willChange action to +/- the CSS property

---
+++ src/lib/actions/will-change.ts
@@ -0,0 +1,63 @@
+export type WillChangeParameters = {
+	properties: string;
+	type: WillChangeTypes;
+};
+export type WillChangeTypes = 'mouse' | 'transition' | 'page-load';
+
+export function willChange(element: HTMLElement, parameters: WillChangeParameters) {
+	const { properties, type } = parameters;
+
+	function setWillChangeProperties() {
+		element.style.willChange = properties;
+	}
+	function resetWillChangeProperties() {
+		element.style.willChange = 'auto';
+	}
+
+	// Event-Listener functions
+	function mouseEnter() {
+		setWillChangeProperties();
+	}
+	function mouseLeave() {
+		resetWillChangeProperties();
+	}
+	function transitionStart() {
+		setWillChangeProperties();
+	}
+	function transitionEnd() {
+		resetWillChangeProperties();
+	}
+
+	// Assign the correct event listeners to the element, depending on the type of will-change
+	function assignEventListeners() {
+		if (type === 'mouse') {
+			element.addEventListener('mouseenter', mouseEnter);
+			element.addEventListener('mouseleave', mouseLeave);
+		} else if (type === 'transition') {
+			element.addEventListener('transitionstart', transitionStart);
+			element.addEventListener('transitionend', transitionEnd);
+		} else if (type === 'page-load') {
+			element.style.willChange = properties;
+		}
+	}
+
+	assignEventListeners();
+
+	return {
+		update(new_parameters: WillChangeParameters) {
+			willChange(element, new_parameters);
+		},
+
+		destroy() {
+			if (type === 'mouse') {
+				element.removeEventListener('mouseenter', mouseEnter);
+				element.removeEventListener('mouseleave', mouseLeave);
+			} else if (type === 'transition') {
+				element.removeEventListener('transitionstart', transitionStart);
+				element.removeEventListener('transitionend', transitionEnd);
+			} else if (type === 'page-load') {
+				element.style.willChange = 'auto';
+			}
+		}
+	};
+}
