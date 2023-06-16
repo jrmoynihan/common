2d452d8cf797815e79aefa0ab63d641a7bc9e3f9
James Moynihan <jrmoynihan@gmail.com>
2023-06-16 10:21:05 -0400

    remove unneeded scss preprocess pass

--- svelte.config.js
+++ svelte.config.js
@@ -16,12 +16,7 @@ const config = {
 
 	// Consult https://github.com/sveltejs/svelte-preprocess
 	// for more information about preprocessors
-	preprocess: [
-		mdsvex(mdsvexConfig),
-		preprocess({
-			scss: true
-		})
-	],
+	preprocess: [mdsvex(mdsvexConfig), preprocess()],
 
 	kit: {
 		adapter: auto(),
