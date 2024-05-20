a51af81213cebe2a72cd6f5162bb3ec0f5c4b1d8
James Moynihan <65916939+jrmoynihan@users.noreply.github.com>
2024-05-20 17:23:26 -0400

    validity events and placeholder dynamic styling

--- src/lib/inputs/TextInput.svelte
+++ src/lib/inputs/TextInput.svelte
@@ -9,9 +9,10 @@
 		dynamic_input_styles?: DynamicStyleParameters;
 		label_element?: HTMLLabelElement;
 		/** Props on the `<label>` element that wraps the input, including the tooltip action and transition directive. */
-		label_props?: ComponentProps<InputLabel>;
-		placeholder_props?: ComponentProps<Placeholder>;
-		children?: Snippet;
+		label_props?: InputLabelProps;
+		/** A binding to the placeholder <div> element */
+		placeholder_element?: HTMLDivElement;
+		placeholder_props?: PlaceholderProps;
 		/** A callback that runs when the confirm button is clicked */
 		onconfirm?: FormEventHandler<HTMLInputElement>;
 		/** A callback that runs when the cancel button is clicked */
@@ -22,31 +23,35 @@
 </script>
 
 <script lang="ts">
-	import { dynamicStyle, type DynamicStyleParameters } from '$actions/dynamic-styles.svelte.js';
+	import { type DynamicStyleParameters } from '$actions/dynamic-styles.svelte.js';
 	import { faCheck, faX } from '@fortawesome/free-solid-svg-icons/index';
-	import { Fa } from '@jrmoynihan/svelte-fa';
-	import { type ComponentProps, type Snippet } from 'svelte';
 	import type {
 		FormEventHandler
 	} from 'svelte/elements';
 	import Input, { type InputProps } from './Input.svelte';
-	import InputLabel from './InputLabel.svelte';
-	import Placeholder from './Placeholder.svelte';
+	import InputButton from './InputButton.svelte';
+	import InputLabel, { type InputLabelProps } from './InputLabel.svelte';
+	import Placeholder, { type PlaceholderProps } from './Placeholder.svelte';
 
 	let {
 		input_element = $bindable(),
 		value = $bindable(),
+		id = crypto?.randomUUID(),
 		show_confirm = true,
 		show_cancel = true,
 		allow_enter_to_confirm = true,
 		dynamic_button_styles,
 		dynamic_input_styles,
+		placeholder_element = $bindable(),
 		placeholder_props = {},
 		label_element = $bindable(),
 		label_props = { ...placeholder_props },
 		children,
 		onconfirm = () => input_element?.blur(),
 		oncancel = clear_input,
+		oninvalid,
+		onvalid,
+		onblur,
 		oninput,
 		...input_attributes
 	}: TextInputProps = $props();
@@ -70,13 +75,28 @@
 
 	function handle_keypress(e: KeyboardEvent) {
 		if (e.key === 'Enter' && allow_enter_to_confirm) confirm(e);
+		placeholder_element?.dispatchEvent(new Event('keypress'));
 	}
 	function handle_input(e: Event & { currentTarget: EventTarget & HTMLInputElement }) {
 		oninput?.(e);
+		placeholder_element?.dispatchEvent(new Event('input'));
 	}
 
 	function handle_click(e: MouseEvent) {
 		confirm(e);
+	}
+	function handle_invalid(e: Event & { currentTarget: EventTarget & HTMLInputElement }) {
+		oninvalid?.(e);
+		placeholder_element?.dispatchEvent(new Event('invalid'));
+	}
+
+	function handle_valid(e: Event & { currentTarget: EventTarget & HTMLInputElement }) {
+		onvalid?.(e);
+		placeholder_element?.dispatchEvent(new Event('valid'));
+	}
+	function handle_blur(e: FocusEvent & { currentTarget: EventTarget & HTMLInputElement }) {
+		onblur?.(e);
+		placeholder_element?.dispatchEvent(new Event('blur'));
 	}
 
 	let valid = $state(true);
@@ -85,35 +105,31 @@
 	// TODO: Use the Sanitizer API: https://web.dev/sanitizer/
 </script>
 
-<InputLabel {...label_props} bind:label_element bind:valid >
+<InputLabel bind:label_element bind:valid {id} {...label_props} >
 	<Input 
 		bind:dynamic_input_styles
 		bind:input_element
 		bind:value
 		bind:valid
+		{id}
+		onblur={handle_blur}
+		onvalid={handle_valid}
+		oninvalid={handle_invalid}
 		onkeypress={handle_keypress}
 		oninput={handle_input}
 		type={'text'}
 		{...input_attributes}
 	/>
 		{#key placeholder_props}
-			<Placeholder {...placeholder_props} />
+			<Placeholder bind:placeholder_element {...placeholder_props} />
 		{/key}
 		{#if children}
 			{@render children()}
 		{/if}
 	<div class="btn-container" class:valid class:show_confirm class:value>
 		{#if show_confirm}
-			<!-- <ButtonRunes 
-				icon_props={{
-					icon: faCheck,
-					color: 'var(--text-input-button-color, buttontext)'
-				}} 
-				attributes={{ onclick: confirm, disabled: !value}}
-				dynamic_styles={dynamic_button_styles}
-				classes={`confirm-btn ${is_valid ? 'valid': ''} ${value ? 'value' : ''}`}
-			/> -->
-			<button
+			<InputButton dynamic_styles={dynamic_button_styles} classes={`confirm-btn ${valid ? 'valid': ''} ${value ? 'value' : ''}`} tabindex={value && valid ? 0 : -1} onclick={handle_click} disabled={!value} icon_props={{icon: faCheck, color: 'var(--text-input-button-color, buttontext)'}}></InputButton>
+			<!-- <button
 				use:dynamicStyle={dynamic_button_styles}
 				class="confirm-btn"
 				class:valid
@@ -123,10 +139,11 @@
 				disabled={!value}
 			>
 				<Fa icon={faCheck} color="var(--text-input-button-color, buttontext)" />
-			</button>
+			</button> -->
 		{/if}
 		{#if show_cancel}
-			<button
+			<InputButton dynamic_styles={dynamic_button_styles} classes={`cancel-btn ${valid ? 'valid': ''} ${value ? 'value' : ''}`} tabindex={value ? 0 : -1} onclick={cancel} disabled={!value} icon_props={{icon: faX, color: 'var(--text-input-button-color, buttontext)'}}></InputButton>
+			<!-- <button
 				use:dynamicStyle={dynamic_button_styles}
 				class="cancel-btn"
 				class:no-confirm={!show_confirm || !valid}
@@ -136,7 +153,7 @@
 				disabled={!value}
 			>
 				<Fa icon={faX} color="var(--text-input-button-color, buttontext)" />
-			</button>
+			</button> -->
 		{/if}
 	</div>
 	<!-- {#if invalid_msg_visible && invalid_msg}
@@ -185,73 +202,52 @@
 			grid-template-rows: 0 1fr;
 		}
 		&:not(.value) {
-			grid-template-rows: none;
-			grid-template-columns: none;
+			grid-template-rows: 0;
+			grid-template-columns: 0;
 		}
 	}
 	@layer button {
-		.cancel-btn, .confirm-btn {
-			--max-width: 1.75rem;
-			box-sizing: content-box;
-			display: grid;
-			place-items: center;
-			max-height: var(--max-width);
-			aspect-ratio: 1 / 1;
-			background-color: field;
-			appearance: textfield;
-			color: var(--text-input-color, fieldtext);
-			border: var(--text-input-button-border, 1px inset buttonborder);
-			padding: var(--text-input-button-padding, calc(var(--max-width) / 3.5));
-			opacity: 0;
-			transition: all 300ms ease;
-			outline: 1px currentColor inset;
-			outline-offset: -1px;
-			border: 0px;
-	
-			&:focus {
-				outline: 1px var(--button-outline-hover-or-focus, -webkit-focus-ring-color) solid;
-			}
-	
-			&.value {
-				opacity: 0.5;
-	
-				&:hover,
-				&:focus-visible {
-					opacity: 1;
-				}
-				&:focus-visible {
-					outline: 1px var(--button-outline-hover-or-focus, -webkit-focus-ring-color) solid;
-				}
-			}
+		:global(:is(.cancel-btn.value, .confirm-btn.value)) {
+			opacity: 0.5;
 		}
-		.cancel-btn {
+		:global(:is(.cancel-btn.value, .confirm-btn.value):where(:hover, :focus-visible)) {
+			opacity: 1;
+		}
+		:global(:is(.cancel-btn, .confirm-btn):where(:focus-visible)) {
+			outline: 1px var(--button-outline-hover-or-focus, -webkit-focus-ring-color) solid;
+		}
+		:global(.cancel-btn) {
 			grid-area: cancel;
 			transform-origin: top right;
 			scale: 1 1;
-			&:not(.value) {
-				scale: 0 1;
-			}
-			&.no-confirm {
-				box-sizing: border-box;
-				margin-bottom: 0;
-				/* padding: calc(var(--max-width) / 3.5) calc(var(--max-width) / 3); */
-				max-height: 100%;
-				height: 100%;
-				justify-self: end;
-				margin-right: 0;
-				aspect-ratio: unset;
-			}
 		}
-		.confirm-btn {
+		:global(.cancel-btn:not(.value)) {
+			scale: 0 1;
+		}
+		:global(.cancel-btn.no-confirm) {
+			box-sizing: border-box;
+			margin-bottom: 0;
+			/* padding: calc(var(--max-width) / 3.5) calc(var(--max-width) / 3); */
+			max-height: 100%;
+			height: 100%;
+			justify-self: end;
+			margin-right: 0;
+			aspect-ratio: unset;
+
+		}
+		
+		:global(.confirm-btn) {
 			grid-area: confirm;
 			transform-origin: top right;
-			&:not(.valid) {
-				pointer-events: none;
-				opacity: 0;
-				scale: 1 0;
-				padding: 0;
-				height: 0;
-			}
+		}
+		:global(.confirm-btn:not(.valid)) {
+			display: none;
+			pointer-events: none;
+			opacity: 0;
+			scale: 1 0;
+			padding: 0;
+			height: 0;
+			
 		}
 
 	}
