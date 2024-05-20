5e677e19e74237766490e0bf4076432cee7598e8
James Moynihan <65916939+jrmoynihan@users.noreply.github.com>
2024-05-20 17:20:17 -0400

    clean up styles by using existing base components

--- src/lib/inputs/NumericInput.svelte
+++ src/lib/inputs/NumericInput.svelte
@@ -1,7 +1,9 @@
 <script context="module" lang="ts">
-	export interface NumericInputProps extends HTMLInputAttributes {
-		numeric_input?: HTMLInputElement | null;
+	export interface NumericInputProps extends InputProps {
+		input_element?: HTMLInputElement;
 		value?: string | number | string[] | null;
+		valid?: boolean;
+		dynamic_input_styles?: DynamicStyleParameters;
 		label_element?: HTMLLabelElement;
 		invalid_msg?: string;
 		show_spinner_buttons?: boolean;
@@ -21,76 +23,61 @@
 	// TODO: add a prop for a custom validity function?
 	// TODO: add a SHIFT/CTRL modifier to allow for larger steps too
 
-	interface SpinnerButtonAttributes extends HTMLButtonAttributes  {
-		classes?: string; // avoids the `class` keyword, but keeps the semantics
-	}
-	type SpinnerButton = SpinnerButtonAttributes & ComponentProps<Fa>
+	type SpinnerButton = InputButtonProps & ComponentProps<Fa>
 </script>
 
 <script lang="ts">
-	import { dynamicStyle, type DynamicStyleParameters } from '$actions/dynamic-styles.svelte.js';
-	import { tooltip, type TooltipProps } from '$actions/tooltip/tooltip.svelte.js';
+	import { type DynamicStyleParameters } from '$actions/dynamic-styles.svelte.js';
+	import { type TooltipProps } from '$actions/tooltip/tooltip.svelte.js';
 	import { faCaretDown, faCaretUp } from '@fortawesome/free-solid-svg-icons';
 	import { Fa } from '@jrmoynihan/svelte-fa';
 	import { type ComponentProps, type Snippet } from 'svelte';
-	import type { HTMLButtonAttributes, HTMLInputAttributes } from 'svelte/elements';
+	import Input, { type InputProps } from './Input.svelte';
+	import InputButton, { type InputButtonProps } from './InputButton.svelte';
 	import InputLabel from './InputLabel.svelte';
 	import Placeholder from './Placeholder.svelte';
 
 	let {
 		value = $bindable(0),
-		numeric_input = $bindable(),
+		valid = $bindable(true),
+		input_element = $bindable(),
 		label_element = $bindable(),
+		dynamic_input_styles = $bindable(),
 		show_spinner_buttons = true,
 		tooltip_options = null,
-		container_styles: dynamic_container_styles,
 		input_styles,
 		placeholder_props,
 		label_props,
 		spinner_button,
 		up_spinner_button,
 		down_spinner_button,
+		children,
 		...input_attributes
 	} : NumericInputProps = $props();
 
-	export const is_valid = $derived(numeric_input?.checkValidity() ?? true);
+	export const is_valid = $derived(input_element?.checkValidity() ?? true);
 	
 </script>
 
-{#snippet default_spinner_button({classes, icon, size, onclick}: SpinnerButton)}
-<button
-	class="spinner {classes}"
-	{onclick}
->
-	<Fa {icon} {size} />
-</button>
+{#snippet default_spinner_button({icon, size, ...button_props}: SpinnerButton)}
+	<InputButton {...button_props}>
+		<Fa {icon} {size} color="var(--text-input-button-color, buttontext)" class="spinner-button-icon" />
+	</InputButton>
 {/snippet}
 
-<InputLabel bind:label_element {...label_props} >
-	<div
-		use:tooltip={tooltip_options ? { ...tooltip_options } : { disabled: true, visible: false }}
-		use:dynamicStyle={dynamic_container_styles}
-		class="numeric-input-container"
-		class:show-spinner-buttons={show_spinner_buttons}
-	>
-		<Placeholder {...placeholder_props} />
-		<!-- TODO: Replace with generic <Input> component or is passing the complex layout/styles too difficult? -->
-		<input
-			use:dynamicStyle={input_styles}
-			bind:this={numeric_input}
-			bind:value
-			type="number"
-			inputmode="numeric"
-			min={0}
-			max={Infinity}
-			step={1}
-			{...input_attributes}
-		/>
-		{#if show_spinner_buttons}
-			{@const max_num = Number(input_attributes?.max ?? Infinity)}
-			{@const min_num = Number(input_attributes?.min ?? 0)}
-			{@const current = Number(value)}
-			{@const step = Number(input_attributes?.step ?? 1)}
+<InputLabel bind:label_element bind:valid {...label_props}  >
+	<Input bind:dynamic_input_styles bind:input_element bind:value bind:valid type="number" inputmode="numeric" min={0} max={Infinity} step={1} {...input_attributes} />
+	<Placeholder {...placeholder_props} />
+	{#if children}
+		{@render children()}
+	{/if}
+	{#if show_spinner_buttons}
+		{@const max_num = Number(input_attributes?.max ?? Infinity)}
+		{@const min_num = Number(input_attributes?.min ?? 0)}
+		{@const current = Number(value)}
+		{@const step = Number(input_attributes?.step ?? 1)}
+
+		<div class="btn-container" >
 			{#if up_spinner_button}
 				{@render up_spinner_button()}
 			{:else if spinner_button}
@@ -102,6 +89,7 @@
 					}
 				}})}
 			{/if}
+
 			{#if down_spinner_button}
 				{@render down_spinner_button()}
 			{:else if spinner_button}
@@ -113,54 +101,73 @@
 					}
 				}})}
 			{/if}
-		{/if}
-	</div>
+
+		</div>
+	{/if}
 </InputLabel>
 
 <style>
-	.numeric-input-container {
-		display: grid;
-		border-radius: 1rem;
-		position: relative;
-		width: var(--numeric-input-container-width, 100%);
-		isolation: isolate; /* Contain the z-index stacking context for the buttons to this container. */
-		&.show-spinner-buttons {
-			grid-template-columns: minmax(0, 1fr) minmax(0, 1.75rem);
-			grid-template-rows: repeat(2, minmax(0, 1fr));
-			grid-template-areas:
-				'input plus '
-				'input minus ';
+	@layer numeric_input {
+		.btn-container {
+			--text-input-button-margin: 0.15rem;
+			--input-border-radius: 1rem;
+			display: grid;
+			grid-template-rows: repeat(auto-fit, minmax(0, 1fr));
+			grid-template-areas: 
+				'plus'
+				'minus';
+			place-items: center;
+			grid-area: input; /* Overlap with the placeholder and input; */
+			place-self: center end;
+			height: 100%;
+			transform-origin: right;
+			margin: 0;
+			border-top-left-radius: 0;
+			border-bottom-left-radius: 0;
+			border-top-right-radius: max(
+				calc(var(--text-input-border-radius, 1em) - var(--text-input-padding, 1.25em)),
+				var(--text-input-border-radius, 1em)
+			);
+			border-bottom-right-radius: max(
+				calc(var(--text-input-border-radius, 1em) - var(--text-input-padding, 1.25em)),
+				var(--text-input-border-radius, 1em)
+			);
+			transition: all 300ms ease;
+			scale: 1 1;
+			z-index: 0;
+			overflow: hidden;
+			isolation: isolate;  /* Contain the z-index stacking context for the buttons to this container. */
 		}
 	}
-	input {
-		position: relative;
-		grid-row: 1 / span 2;
-		grid-column: 1 / span 2;
-		&::-webkit-inner-spin-button,
-		&::-webkit-outer-spin-button {
-			-webkit-appearance: none;
-			appearance: none;
+
+	@layer input {
+		:global(input[type="number"]) {
+			appearance: textfield;
+			-moz-appearance: textfield;
+			text-align: center;
+			padding: 1rem;
+			padding-right: 1.75rem;
+			border-radius: var(--input-border-radius, 1rem);
 		}
-		appearance: textfield;
-		-moz-appearance: textfield;
-		text-align: center;
-		padding: 1rem;
-		padding-right: 1.75rem;
-		border-radius: var(--input-border-radius, 1rem);
 	}
-	.spinner {
-		z-index: var(--spinner-z-index, var(--above, 1));
-		width: 1.75rem;
-		height: 100%;
-		background: var(--spinner-background);
-		color: var(--spinner-color);
+	:global([data-theme="light"] button.button:focus .spinner-button-icon path) {
+		fill: var(--button-outline-hover-or-focus, royalblue);
 	}
-	.plus {
-		grid-area: plus;
-		border-top-right-radius: inherit;
+
+	:global([data-theme="dark"] button.button:focus .spinner-button-icon path) {
+		fill: var(--button-outline-hover-or-focus, lightskyblue);
 	}
-	.minus {
-		grid-area: minus;
-		border-bottom-right-radius: inherit;
+
+	@layer button{
+		:global(.plus){
+			grid-area: plus;
+			border-top-right-radius: inherit;
+			transform-origin: top right;
+		}
+		:global(.minus){
+			grid-area: minus;
+			border-bottom-right-radius: inherit;
+			transform-origin: top right;
+		}
 	}
 </style>
