8fdd0bab2e37dc47cfe3452a6c28fac7b0de8926
James Moynihan <65916939+jrmoynihan@users.noreply.github.com>
2024-05-27 15:20:57 -0400

    restrict datalist types to strings, numbers, and Records.

--- src/lib/inputs/DatalistTextInput.svelte
+++ src/lib/inputs/DatalistTextInput.svelte
@@ -2,11 +2,13 @@
 	import type { ComponentProps } from 'svelte';
 	import InputLabel from './InputLabel.svelte';
 	import TextInput, { type TextInputProps } from './TextInput.svelte';
-	import type { DatalistOption } from './types';
+	// import type { DatalistOption } from './types';
 
-	interface DatalistTextInputProps extends TextInputProps {
-		datalist: DatalistOption[];
+	export interface DatalistTextInputProps extends TextInputProps {
+		datalist: string[] | number[] | Record<string, unknown>[];
 		value?: unknown;
+		value_key?: string;
+		label_key?: string;
 		label_props?: ComponentProps<InputLabel>;
 		label_element?: HTMLLabelElement;
 	}
@@ -16,6 +18,8 @@
 		datalist = $bindable(),
 		value = $bindable(),
 		label_element = $bindable(),
+		value_key = 'value',
+		label_key = 'label',
 		label_props,
 		...text_input_props
 	}: DatalistTextInputProps = $props();
@@ -31,11 +35,21 @@
 	aria-haspopup={'listbox'}
 	aria-label={list}
 	{list}
+	type={typeof datalist[0] === 'number' ? 'number' : 'text'}
 	{...text_input_props}
 >
 	<datalist id={list} tabindex="-1">
-		{#each datalist as { value, label }}
-			<option {value}>{label ?? value}</option>
+		{#each datalist as list_item}
+			{@const is_object = typeof list_item === 'object'}
+			<option value={is_object ? list_item[value_key] : list_item}>
+				{#if is_object && label_key in list_item}
+					{list_item[label_key]}
+				{:else if is_object && value_key in list_item}
+					{list_item[value_key]}
+				{:else if !is_object}
+					{list_item}
+				{/if}
+			</option>
 		{/each}
 	</datalist>
 </TextInput>
