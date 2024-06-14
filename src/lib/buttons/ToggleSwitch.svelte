6653c3bf07e3576ecbbf01e8f8aacf8ac7c7f7a7
James Moynihan <65916939+jrmoynihan@users.noreply.github.com>
2024-06-14 06:08:48 -0400

    fix: convert toggle back to displayed (but hidden) input; improve styling

--- src/lib/buttons/ToggleSwitch.svelte
+++ src/lib/buttons/ToggleSwitch.svelte
@@ -1,5 +1,5 @@
 <script context="module" lang="ts">
-	export interface ToggleSwitchProps<T> extends ButtonProps<T> {
+	export interface ToggleSwitchProps extends HTMLInputAttributes {
 		/** The checked state of the toggle. Defaults to `false`. */
 		checked?: boolean;
 		/** The disabled state of the toggle. Defaults to `false`. */
@@ -12,8 +12,6 @@
 		label_text?: string;
 		/** The text to display in the slider. */
 		slider_text?: string;
-		/** The attributes to apply to the `<input type="checkbox">` element. */
-		input_attributes?: HTMLInputAttributes;
 		/** The attributes to apply to the `<span class="round slider">` element. */
 		slider_attributes?: HTMLAttributes<HTMLSpanElement>;
 		/** A callback that is triggered when the toggle is toggled.  Defaults to `() => { checked = !checked }` which allows a simple binding to the `checked` prop. */
@@ -21,87 +19,62 @@
 	}
 </script>
 
-<script lang="ts" generics="T">
+<script lang="ts">
 	import { dynamic_style, type DynamicStyleParameters } from '$actions/dynamic-styles.svelte.js';
 	import type { HTMLAttributes, HTMLInputAttributes } from 'svelte/elements';
-	import ButtonRunes, { type ButtonProps } from './Button_Runes.svelte';
 
 	let {
-		checked = $bindable(false),
-		disabled = $bindable(false),
+		checked = $bindable(),
+		disabled = $bindable(),
 		label_dynamic_styles,
 		label_position = 'before',
 		label_text,
 		slider_text,
-		ontoggle = default_on_toggle,
-		input_attributes,
 		slider_attributes,
+		ontoggle,
 		children,
-		...button_props
-	} : ToggleSwitchProps<T> = $props()
+		...input_attributes
+	} : ToggleSwitchProps = $props()
 
-	function default_on_toggle() {
-		checked = !checked;
-	}
+	const id = crypto?.randomUUID();
 
 </script>
 
-{#snippet label(label_text: string)}
-	<label
-		use:dynamic_style={label_dynamic_styles}
-		class="label-text pointer"
-		for={'toggle'}
-	>	
-		{label_text}
-	</label>
-{/snippet}
-
-<ButtonRunes
-	bind:disabled
-	--button-hover-background={'var(--toggle-button-hover-background, inherit)'}
-	onpointerdown={ontoggle}
-	role={'switch'}
-	style={`border: 0;
-			padding: 0.2rem; 
-			display: grid; 
-			gap: 0.5rem; 
-			scale: 1em;
-			background-color: var(--toggle-button-background, inherit);
-			color: var(--toggle-button-color, inherit);
-			${button_props?.style}`}
-	classes={`toggle ${button_props?.classes}`}
-	{...button_props}
->
+<label
+	use:dynamic_style={label_dynamic_styles}
+	class="label-text pointer"
+	for={`toggle-${id}`}
+>	
 	{#if label_position === 'before'}
 		{#if children}
 			{@render children?.()}
 		{:else if label_text}
-			{@render label?.(label_text)}
+			{label_text}
 		{/if}
-	{/if}
+	{/if} 
+	<!-- NOTE: Subtle fix made by changing this to on:change event instead of on:click -->
+	<input
+		id={`toggle-${id}`}
+		bind:checked
+		type="checkbox"
+		aria-checked={checked}
+		{disabled}
+		onchange={ontoggle}
+		{...input_attributes}
+	/>
 	<span class="switch">
-		<!-- NOTE: Subtle fix made by changing this to on:change event instead of on:click -->
-		<input
-			bind:checked
-			type="checkbox"
-			aria-checked={checked}
-			{disabled}
-			onchange={ontoggle}
-			{...input_attributes}
-		/>
 		<span class="slider round" data-slider-text={slider_text} {...slider_attributes}></span>
 	</span>
 	{#if label_position === 'after'}
 		{#if children}
 			{@render children?.()}
 		{:else if label_text}
-			{@render label?.(label_text)}
+			{label_text}
 		{/if}
 	{/if}
-</ButtonRunes>
+</label>
 
 <style>
-	
 	/* The switch - the box around the slider */
 	.switch,
 	.label-text {
@@ -111,6 +84,24 @@
 		align-self: center;
 		width: calc(var(--toggle-scale) * 3);
 	}
+	.label-text{
+		padding-inline: 0.25rem;
+		width: auto;
+		&:has(input:focus-visible){
+			outline: solid 1px var(--toggle-button-focus-color, var(--toggleBgColorActive, -webkit-focus-ring-color));
+		}
+		&:hover, &:focus-visible {
+			color: var(--label-text-hover-color, inherit);
+		}
+		&:hover:has(input){
+			& > .switch > .slider {
+				outline: 1px solid fieldtext;
+			}
+			&:checked > .switch > .slider {
+					background-color: var(--toggleBgColorActiveHovered, hsl(207, 90%, 34%));
+				}
+		}
+	}
 	.switch {
 		background: transparent;
 		box-shadow: none;
@@ -118,7 +109,7 @@
 		border-radius: 1em;
 		height: var(--toggle-height, calc(var(--toggle-scale) * 1.7));
 	}
-	/* The slider */
+	/* The round slider */
 	.slider {
 		position: absolute;
 		cursor: pointer;
@@ -127,8 +118,16 @@
 		right: 0;
 		bottom: 0;
 		background-color: var(--inactiveBgColor, hsl(0, 0%, 27%));
-		-webkit-transition: all 0.4s ease-in-out;
-		transition: all 0.4s ease-in-out;
+		transition: outline;
+		transition-delay: 1s;
+		outline-offset: 1.5px;
+		&.round{
+			border-radius: var(--toggle-height, calc(var(--toggle-scale) * 1.7));
+			padding: 1px;
+			&:before {
+				border-radius: 50%;
+			}
+		}
 	}
 	.slider:before {
 		position: absolute;
@@ -148,48 +147,25 @@
 	.slider:hover {
 		background-color: var(--inactiveBgColorDarken, hsl(0, 0%, 27%));
 	}
-
-	input {
-		appearance: none;
-		visibility: hidden;
+	
+	input{
 		height: 0;
 		width: 0;
-		display: none;
-	}
 
-	input:checked + .slider {
-		background-color: var(--toggleBgColorActive, hsl(207, 90%, 54%));
+		&:checked + .switch > .slider {
+			background-color: var(--toggleBgColorActive, hsl(207, 90%, 54%));
+			&:hover {
+				background-color: var(--toggleBgColorActiveHovered, hsl(207, 90%, 34%));
+			}
+			&:before{
+				translate: var(--slider-transform, 130%) 0 0;
+			}
+		}
+		&:disabled + .switch > .slider {
+			background-color: var(--toggleBgColorActiveHovered, hsl(207, 90%, 34%));
+		}
 	}
-	input:checked + .slider:before {
-		translate: var(--slider-transform, 130%) 0 0;
-	}
-	input:checked + .slider:hover {
-		background-color: var(--toggleBgColorActiveHovered, hsl(207, 90%, 34%));
-	}
-	input:disabled + .slider {
-		background-color: var(--toggleBgColorActiveHovered, hsl(207, 90%, 34%));
-	}
-	input:focus + .slider {
-		box-shadow: 0 0 2px 3px var(--toggleBgColorActiveHovered, hsl(207, 90%, 34%));
-		transition: all 100ms ease-in-out;
-	}
-
-	/* Rounded sliders */
-	.slider.round {
-		border-radius: var(--toggle-height, calc(var(--toggle-scale) * 1.7));
-		padding: 1px;
-	}
-	.slider.round:before {
-		border-radius: 50%;
-	}
-	.label-text {
-		max-width: none;
-		width: auto;
-	}
-	.label-text:hover,
-	.label-text:focus {
-		color: var(--label-text-hover-color, inherit);
-	}
+	
 	.pointer {
 		cursor: pointer;
 	}
