6c5a90ee8eb6832bb5d09065a40d57c06a8add80
James Moynihan <65916939+jrmoynihan@users.noreply.github.com>
2024-06-14 21:14:48 -0400

    fix: remove flex from the label container

--- src/lib/buttons/ToggleSwitch.svelte
+++ src/lib/buttons/ToggleSwitch.svelte
@@ -34,24 +34,19 @@
 		ontoggle,
 		children,
 		...input_attributes
-	} : ToggleSwitchProps = $props()
+	}: ToggleSwitchProps = $props();
 
 	const id = crypto?.randomUUID();
-
 </script>
 
-<label
-	use:dynamic_style={label_dynamic_styles}
-	class="label-text pointer"
-	for={`toggle-${id}`}
->	
+<label use:dynamic_style={label_dynamic_styles} class="label-text pointer" for={`toggle-${id}`}>
 	{#if label_position === 'before'}
 		{#if children}
 			{@render children?.()}
 		{:else if label_text}
 			{label_text}
 		{/if}
-	{/if} 
+	{/if}
 	<!-- NOTE: Subtle fix made by changing this to on:change event instead of on:click -->
 	<input
 		id={`toggle-${id}`}
@@ -80,29 +75,31 @@
 	.label-text {
 		--toggle-scale: 1rem;
 		position: relative;
-		display: flex;
+		/* display: flex; */
 		align-self: center;
 		align-items: center;
 		width: calc(var(--toggle-scale) * 3);
 	}
-	.label-text{
+	.label-text {
 		padding-inline: 0.25rem;
 		width: auto;
 		gap: 0.2rem;
 		flex-wrap: wrap;
-		&:has(input:focus-visible){
-			outline: solid 1px var(--toggle-button-focus-color, var(--toggleBgColorActive, -webkit-focus-ring-color));
+		&:has(input:focus-visible) {
+			outline: solid 1px
+				var(--toggle-button-focus-color, var(--toggleBgColorActive, -webkit-focus-ring-color));
 		}
-		&:hover, &:focus-visible {
+		&:hover,
+		&:focus-visible {
 			color: var(--label-text-hover-color, inherit);
 		}
-		&:hover:has(input){
+		&:hover:has(input) {
 			& > .switch > .slider {
 				outline: 1px solid fieldtext;
 			}
 			&:checked > .switch > .slider {
-					background-color: var(--toggleBgColorActiveHovered, hsl(207, 90%, 34%));
-				}
+				background-color: var(--toggleBgColorActiveHovered, hsl(207, 90%, 34%));
+			}
 		}
 	}
 	.switch {
@@ -124,7 +121,7 @@
 		transition: outline;
 		transition-delay: 1s;
 		outline-offset: 1.5px;
-		&.round{
+		&.round {
 			border-radius: var(--toggle-height, calc(var(--toggle-scale) * 1.7));
 			padding: 1px;
 			&:before {
@@ -150,8 +147,8 @@
 	.slider:hover {
 		background-color: var(--inactiveBgColorDarken, hsl(0, 0%, 27%));
 	}
-	
-	input{
+
+	input {
 		height: 0;
 		width: 0;
 
@@ -160,7 +157,7 @@
 			&:hover {
 				background-color: var(--toggleBgColorActiveHovered, hsl(207, 90%, 34%));
 			}
-			&:before{
+			&:before {
 				translate: var(--slider-transform, 130%) 0 0;
 			}
 		}
@@ -168,7 +165,7 @@
 			background-color: var(--toggleBgColorActiveHovered, hsl(207, 90%, 34%));
 		}
 	}
-	
+
 	.pointer {
 		cursor: pointer;
 	}
