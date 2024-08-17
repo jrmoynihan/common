28d2fcfd110dfbd4035691ea6530c1ed150628e7
James Moynihan <65916939+jrmoynihan@users.noreply.github.com>
2024-08-16 23:48:24 -0400

    fix: correct overflow container and transition effect

--- src/lib/wrappers/Accordion.svelte
+++ src/lib/wrappers/Accordion.svelte
@@ -1,23 +1,23 @@
-<script context='module' lang='ts'>
+<script context="module" lang="ts">
 	export interface AccordionProps extends HTMLButtonAttributes {
 		/** A snippet to provide a custom summary section instead of just passing the `summary_text`. */
-		summary?: Snippet | string,
+		summary?: Snippet | string;
 		/** The parameters of the `<summary>` tooltip. */
-		summary_tooltip_props?: TooltipProps,
+		summary_tooltip_props?: TooltipProps;
 		/** Attributes to apply to the `<summary>` element. */
-		summary_attributes?: HTMLAttributes<HTMLElement>,
+		summary_attributes?: HTMLAttributes<HTMLElement>;
 		/** (Bindable) The open state of the accordion. */
-		open?: boolean
+		open?: boolean;
 		/** The parameters of the transition. */
-		transition_props?: ComponentProps<TransitionRunes>,
+		transition_props?: ComponentProps<TransitionRunes>;
 		/** The position of the expand icon. */
-		expand_icon_position?: 'left' | 'right' | 'none',
+		expand_icon_position?: 'left' | 'right' | 'none';
 		/** Props to apply to the expand/collapse icon's `<Fa>` component. */
-		icon_props?: ComponentProps<Fa>,
+		icon_props?: ComponentProps<Fa>;
 		/** The rotation of the open icon. */
-		closed_icon_rotation?: number
+		closed_icon_rotation?: number;
 		/** The rotation of the closed icon. */
-		open_icon_rotation?: number
+		open_icon_rotation?: number;
 	}
 </script>
 
@@ -29,7 +29,9 @@
 	import type { HTMLAttributes, HTMLButtonAttributes } from 'svelte/elements';
 	import TransitionRunes from './Transition_Runes.svelte';
 
-	export const toggle = () => {open = !open};
+	export const toggle = () => {
+		open = !open;
+	};
 
 	let {
 		summary,
@@ -38,36 +40,36 @@
 		children,
 		open = $bindable(false),
 		transition_props = {
-			slide_transition_parameters: { duration: 300, easing: cubicInOut },
+			slide_transition_parameters: { duration: 300, easing: cubicInOut }
 		},
 		expand_icon_position = 'right',
 		icon_props,
 		closed_icon_rotation = 90,
 		open_icon_rotation = -90,
 		...button_attributes
-	} : AccordionProps = $props()
+	}: AccordionProps = $props();
 </script>
 
 {#snippet icon(icon_props)}
 	<Fa
-	class={'fa-CaretDown'}
-	rotate={open ? open_icon_rotation : closed_icon_rotation}
-	{...icon_props}
+		class={'fa-CaretDown'}
+		rotate={open ? open_icon_rotation : closed_icon_rotation}
+		{...icon_props}
 	/>
 {/snippet}
 
 {#snippet default_expand_icon()}
 	<svg
-	class:open
-	style="tran"
-	width="20"
-	height="20"
-	fill="none"
-	stroke-linecap="round"
-	stroke-linejoin="round"
-	stroke-width="2"
-	viewBox="0 0 24 24"
-	stroke="currentColor"><path d="M9 5l7 7-7 7" /></svg
+		class:open
+		style="tran"
+		width="20"
+		height="20"
+		fill="none"
+		stroke-linecap="round"
+		stroke-linejoin="round"
+		stroke-width="2"
+		viewBox="0 0 24 24"
+		stroke="currentColor"><path d="M9 5l7 7-7 7" /></svg
 	>
 {/snippet}
 
@@ -77,14 +79,14 @@
 	onclick={toggle}
 	aria-expanded={open}
 	{...button_attributes}
->	
+>
 	<summary
-	use:tooltip={summary_tooltip_props}
-	class:left-icon={expand_icon_position === 'left'}
-	class:right-icon={expand_icon_position === 'right'}
-	class:no-icon={expand_icon_position === 'none'}
-	{...summary_attributes}
-		>
+		use:tooltip={summary_tooltip_props}
+		class:left-icon={expand_icon_position === 'left'}
+		class:right-icon={expand_icon_position === 'right'}
+		class:no-icon={expand_icon_position === 'none'}
+		{...summary_attributes}
+	>
 		{#if expand_icon_position === 'left'}
 			{#if icon_props}
 				{@render icon(icon_props)}
@@ -98,7 +100,7 @@
 		{:else}
 			{summary}
 		{/if}
-	
+
 		{#if expand_icon_position === 'right'}
 			{#if icon_props}
 				{@render icon(icon_props)}
@@ -106,55 +108,73 @@
 				{@render default_expand_icon()}
 			{/if}
 		{/if}
-</summary>
+	</summary>
 
 	<!--This prevents the accordion from closing when the button's content is clicked, similar to a <details> element-->
 	<!--svelte-ignore a11y_no_static_element_interactions -->
 	<!--svelte-ignore a11y_click_events_have_key_events-->
-	<div class="content" onclick={(e)=>{e.stopPropagation()}}>
-		{@render children?.()}
+	<div
+		class="content"
+		onclick={(e) => {
+			e.stopPropagation();
+		}}
+	>
+		<div class="content-overflow-container">
+			{@render children?.()}
+		</div>
 	</div>
 </button>
 
 <style>
-	@layer accordion{
+	@layer accordion {
 		.accordion-container {
 			--duration: 400ms;
 			appearance: none;
 			box-sizing: border-box;
 			display: grid;
-			grid-template-rows: max-content 0fr;
+			grid-template-rows: auto auto;
 			color: inherit;
 			font: inherit;
 			cursor: pointer;
 			margin: 1rem;
 			padding: 1rem;
 			border-radius: 1rem;
-			transition: grid-template-rows 400ms;
 			border: var(--accordion-button-border, none);
 			align-items: unset;
-				box-sizing: border-box;
+			box-sizing: border-box;
 			&[aria-expanded='true'] {
 				border-radius: 1rem 1rem 0 0;
-				grid-template-rows: max-content 1fr;
+				& > .content {
+					grid-template-rows: 1fr;
+					opacity: 1;
+				}
 			}
 			& > .content {
-				overflow: hidden;
-				grid-row: 2 / span 2;
+				grid-row: 2;
+				display: grid;
+				grid-template-rows: 0fr;
+				opacity: 0;
+				transition:
+					grid-template-rows 400ms ease,
+					opacity 400ms ease;
+				& > .content-overflow-container {
+					display: grid;
+					overflow: hidden;
+				}
 			}
 		}
 		summary {
-			display:grid ;
+			display: grid;
 			align-items: center;
 			grid-row: 1;
-			
+
 			&.right-icon {
 				grid-template-columns: 1fr auto;
 			}
 			&.left-icon {
 				grid-template-columns: auto 1fr;
 			}
-		
+
 			&.no-icon {
 				grid-template-columns: 1fr;
 			}
@@ -162,11 +182,9 @@
 		svg {
 			transition: rotate var(--duration, 400ms) var(--transition-speed, ease);
 			rotate: var(--icon-closed-rotation, 0deg);
-			&.open{
+			&.open {
 				rotate: var(--icon-open-rotation, 90deg);
 			}
 		}
-
 	}
-
 </style>
