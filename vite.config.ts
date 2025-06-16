8c031add882a1bd1b401d16859a49f57a68d1488
jrmoynihan <jrmoynihan@gmail.com>
2025-06-16 16:06:53 -0400

    add tailwindcss

---
+++ vite.config.ts
@@ -0,0 +1,29 @@
+import { sveltekit } from '@sveltejs/kit/vite';
+import examples from 'mdsvexamples/vite';
+import path from 'node:path';
+import { kitRoutes } from 'vite-plugin-kit-routes';
+import tailwindcss from '@tailwindcss/vite';
+
+const $root = path.resolve(__dirname, './src');
+const $lib = path.resolve($root, './lib');
+const $routes = path.resolve($root, './routes');
+const $scripts = path.resolve($lib, './scripts');
+const $actions = path.resolve($lib, './actions');
+
+/** @type {import('vite').UserConfig} */
+const config = {
+	plugins: [examples, tailwindcss(), sveltekit(), kitRoutes()],
+	resolve: {
+		alias: {
+			$lib,
+			$routes,
+			$scripts,
+			$actions
+		}
+	},
+	ssr: {
+		noExternal: ['@fortawesome/free-solid-svg-icons', '@fortawesome/free-brands-svg-icons']
+	}
+};
+
+export default config;
