52f9ffb591183f9bf4a6140842ba6398d88882a7
jrmoynihan <jrmoynihan@gmail.com>
2025-06-16 16:34:20 -0400

    better generics/types for select options

--- src/lib/inputs/Select.svelte
+++ src/lib/inputs/Select.svelte
@@ -1,43 +1,48 @@
 <script lang="ts" module>
 	// SelectOption: a single option
-	export interface SelectOption<T = unknown> {
-		value: T;
+	export interface SelectOption<T, V = T> extends HTMLOptionAttributes {
+		value: V;
 		label?: string;
 		disabled?: boolean;
 	}
 
 	// SelectOptionGroup: a group of options
-	export interface SelectOptionGroup<T = unknown> {
+	export interface SelectOptionGroup<T, V = T> extends HTMLOptgroupAttributes {
 		label: string;
-		options: Array<SelectOption<T> | SelectOptionGroup<T>>;
+		options: Array<SelectOption<T, V> | SelectOptionGroup<T, V>>;
 		disabled?: boolean;
 	}
 
 	// The options prop: array of options or groups
-	export type SelectOptions<T> = Array<SelectOption<T> | SelectOptionGroup<T>>;
+	export type SelectOptions<T, V> = SelectOption<T, V>[] | SelectOptionGroup<T, V>[];
 
-	export interface SelectProps<T> extends HTMLSelectAttributes {
-		value?: T;
+	export interface SelectProps<T, K extends keyof T = keyof T, V extends T | K = T>
+		extends HTMLSelectAttributes {
+		value?: V;
 		id?: string | null;
 		/** A snippet of HTML for the `<optgroup>` of the `<select>`.  This should also include logic for how to render a single `<option>` vs subgroups. */
-		group_snippet?: Snippet<[SelectOptionGroup<T>]>;
+		group_snippet?: Snippet<[SelectOptionGroup<T, V>]>;
 		/** A snippet for how to render a single `<option>` in the `<select>`.*/
-		option_snippet?: Snippet<[SelectOption<T>]>;
-		options?: SelectOptions<T>;
+		option_snippet?: Snippet<[SelectOption<T, V>]>;
+		options?: SelectOptions<T, V> | T[];
 		dynamic_select_styles?: DynamicStyleParameters;
 		input_label_props?: InputLabelProps<T>;
-		value_key?: string;
-		label_key?: string;
+		value_key?: K;
+		label_key?: K;
 		label?: Snippet<[any]>;
 		children?: Snippet;
 		placeholder_props?: PlaceholderProps;
 	}
 </script>
 
-<script lang="ts" generics="T">
+<script lang="ts" generics="T, K extends keyof T = keyof T, V extends T | K = T">
 	import { dynamic_style, type DynamicStyleParameters } from '$actions/dynamic-styles.svelte.js';
 	import type { Snippet } from 'svelte';
-	import type { HTMLSelectAttributes } from 'svelte/elements';
+	import type {
+		HTMLOptgroupAttributes,
+		HTMLOptionAttributes,
+		HTMLSelectAttributes
+	} from 'svelte/elements';
 	import InputLabel, { type InputLabelProps } from './InputLabel.svelte';
 	import Placeholder, { type PlaceholderProps } from './Placeholder.svelte';
 
@@ -50,27 +55,44 @@
 		children,
 		input_label_props,
 		placeholder_props = {},
-		value_key = 'value',
-		label_key = 'label',
+		value_key,
+		label_key,
 		id = crypto.randomUUID(),
 		...select_attributes
-	}: SelectProps<T> = $props();
+	}: SelectProps<T, K, V> = $props();
 
 	// TODO: Use the Sanitizer API: https://web.dev/sanitizer/
 </script>
 
-{#snippet default_option(item: SelectOption<T>)}
-	{@const { value, label, disabled } = item}
-	<option {value} {disabled}>
-		{label ?? value}
-	</option>
+{#snippet default_option(item: SelectOption<T, V> | T)}>
+	{#if item instanceof Object && value_key !== undefined && value_key in item}
+		{@const value = item[value_key as keyof typeof item]}
+		{@const label =
+			label_key !== undefined && label_key in item ? item[label_key as keyof typeof item] : value}
+		{@const { disabled } = item}
+		<option {value} {disabled}>
+			{label}
+		</option>
+	{:else if item instanceof Object}
+		{@const { disabled } = item}
+		{@const value = item}
+		{@const label =
+			label_key !== undefined && label_key in item ? item[label_key as keyof typeof item] : value}
+		<option {value} {disabled}>
+			{label}
+		</option>
+	{:else}
+		<option value={item}>
+			{item}
+		</option>
+	{/if}
 {/snippet}
 
-{#snippet default_group(group: SelectOptionGroup<T>)}
+{#snippet default_group(group: SelectOptionGroup<T, V>)}
 	{@const { label, options, disabled } = group}
 	<optgroup {label} {disabled}>
 		{#if 'options' in group}
-			{#each group.options as suboption}
+			{#each options as suboption}
 				{#if 'options' in suboption}
 					{@render default_group(suboption)}
 				{:else}
@@ -96,17 +118,10 @@
 			{@render children()}
 		{:else}
 			{#each options as opt}
-				{#if 'options' in opt}
-					{@render group_snippet(opt)}
+				{#if opt instanceof Object && 'options' in opt}
+					{@render group_snippet(opt as unknown as SelectOptionGroup<T, V>)}
 				{:else}
-					{@render option_snippet(opt)}
-				{/if}
-			{/each}
-			{#each options as opt}
-				{#if 'options' in opt}
-					{@render group_snippet(opt)}
-				{:else}
-					{@render option_snippet(opt)}
+					{@render option_snippet(opt as SelectOption<T, V>)}
 				{/if}
 			{/each}
 		{/if}
