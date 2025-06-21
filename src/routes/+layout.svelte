1c360acfb77d535320053a131529753a4978e8aa
jrmoynihan <jrmoynihan@gmail.com>
2025-06-21 15:47:53 -0400

    moved root styles to app stylesheet

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -58,40 +58,10 @@
 
 <!-- <SvelteToast /> -->
 
-<style lang="scss">
-	:root {
-		font-family:
-			Roboto,
-			Oxygen,
-			Ubuntu,
-			Cantarell,
-			'Open Sans',
-			'Helvetica Neue',
-			-apple-system,
-			BlinkMacSystemFont,
-			'Segoe UI',
-			sans-serif;
-		background-color: var(--background);
-		color: var(--text);
-		--tooltip-color: black;
-		--tooltip-font-weight: 400;
-		--tooltip-font-size: 12px;
-		--link-font-size: 1.5rem;
-		--link-border-radius: 2rem;
-		--link-background-color: hsl(195, 61%, 34%);
-		--link-background-value: 195, 61%, 34%;
-		--link-hover-box-shadow: 0 0 4px 1px orange;
-		--link-hover-background-color: hsl(195, 61%, 44%);
-		--current-nav-page-box-shadow: 0 0 14px 4px orange;
-		--current-nav-page-background-color: hsl(195, 61%, 44%);
-		--orange: hsla(39, 100%, 50%, 1);
-		--spinner-background: var(--orange);
-		--spinner-color: black;
-	}
-
+<style>
 	.app-container {
 		display: grid;
-		grid-template-columns: minmax(0, 1fr); // prevent overflow;
+		grid-template-columns: minmax(0, 1fr);
 		transition: padding-left 400ms ease-out;
 		will-change: padding-left;
 	}
@@ -108,12 +78,11 @@
 		right: 0;
 	}
 	h1 {
-		margin: 2rem auto;
-		max-width: max-content;
 		font-style: italic;
 		font-weight: 800;
-		font-size: 5vh;
-		font-family: 'Fira Code iScript';
+		font-family:
+			'Fira Code iScript', 'Roboto', 'Oxygen', 'Ubuntu', 'Cantarell', 'Open Sans', 'Helvetica Neue',
+			'-apple-system', 'BlinkMacSystemFont', 'Segoe UI', sans-serif;
 		text-align: center;
 	}
 	main {
