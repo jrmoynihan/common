6f511d704c47b22016093b62babd6c485f69599d
jrmoynihan <jrmoynihan@gmail.com>
2025-06-20 23:13:58 -0400

    format-only change

--- src/lib/wrappers/MiniDialog.svelte
+++ src/lib/wrappers/MiniDialog.svelte
@@ -1,28 +1,26 @@
-<script module lang='ts'>
-    export interface MiniDialogProps<T> extends Omit<FullDialogProps<T>, 'dialog'> {
-        dialog?: FullDialog<T>;
-    }
+<script module lang="ts">
+	export interface MiniDialogProps<T> extends Omit<FullDialogProps<T>, 'dialog'> {
+		dialog?: FullDialog<T>;
+	}
 </script>
 
-<script lang='ts' generics="T">
+<script lang="ts" generics="T">
 	import FullDialog, { type FullDialogProps } from './FullDialog.svelte';
 
+	let {
+		children = undefined,
+		dialog = $bindable(),
+		...dialog_props
+	}: MiniDialogProps<T> = $props();
 
-    let {
-        children = undefined,
-        dialog = $bindable(),
-        ...dialog_props
-    } : MiniDialogProps<T> = $props();
-
-    export const open = () =>{
-        dialog?.open();
-    }
-    export const close = () => {
-        dialog?.close();
-    }
+	export const open = () => {
+		dialog?.open();
+	};
+	export const close = () => {
+		dialog?.close();
+	};
 </script>
 
-
 <FullDialog bind:this={dialog} {...dialog_props} mode={'mini'} blur={null}>
-    {@render children?.()}
+	{@render children?.()}
 </FullDialog>
