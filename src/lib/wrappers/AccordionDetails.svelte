c53cfb066f8d447358915f6ae496d4114851a88f
jrmoynihan <jrmoynihan@gmail.com>
2025-10-17 15:16:32 -0400

    Simplify accordion to use native CSS/HTML and fewer props. Removed native transition runes (allow parent to insert it instead)

--- src/lib/wrappers/AccordionDetails.svelte
+++ src/lib/wrappers/AccordionDetails.svelte
@@ -9,33 +9,26 @@
 
 <script module lang="ts">
 	export interface AccordionDetailsProps extends HTMLDetailsAttributes {
-		/** How many degrees to rotate the icon when closed. Defaults to 0. */
-		closed_icon_rotation?: number;
-		/** The Iconify icon props. */
+		/** A snippet to provide a custom icon or a string for an Iconify icon. */
+		icon?: Snippet | string;
+		/** Props to apply to the expand/collapse icon's component. */
 		icon_props?: IconProps;
-		/** A custom icon snippet. */
-		custom_icon?: Snippet;
 		/** Attributes for the `<summary>` element. */
 		summary_attributes?: HTMLAttributes<HTMLElement>;
 		/** The position of the expand icon. Defaults to `right`. */
-		expand_icon_position?: 'left' | 'right' | 'none';
+		icon_position?: 'left' | 'right';
 		/** The maximum height of the content when closed. Defaults to `0`. */
 		max_height_closed?: string;
 		/** The maximum height of the content when open. Defaults to `100%`. */
 		max_height_open?: string;
 		/** Whether the accordion is open. Defaults to `false`. */
 		open?: boolean;
-		/** How many degrees to rotate the icon when open. Defaults to `90`. */
-		open_icon_rotation?: number;
+		/** A callback function to be called when the accordion is toggled. */
+		on_toggle?: (open: boolean) => void;
 		/** The summary of the accordion. */
 		summary?: string | Snippet;
-		/**
-		 * Parameters for the content transition. Defaults to `{ slide_transition_parameters: { duration: '500ms', easing: 'ease' } }`.
-		 *  Available transitions are `fly`, `fade`, `blur`, `slide`, and `scale`. See the {@link TransitionNative_Runes.svelte} component.
-		 **/
-		transition_props?: TransitionNativeProps;
-		/** If only details in a group should open at a time, set a name for the group */
-		group_name?: string;
+		/** Whether to show the marker. Defaults to `true`. */
+		show_icon?: boolean;
 	}
 </script>
 
@@ -43,135 +36,67 @@
 	import Icon, { type IconProps } from '@iconify/svelte';
 	import type { Snippet } from 'svelte';
 	import type { HTMLAttributes, HTMLDetailsAttributes } from 'svelte/elements';
-	import TransitionNativeRunes, {
-		type TransitionNativeProps
-	} from './TransitionNative_Runes.svelte';
 
 	let {
-		closed_icon_rotation = 0,
-		children,
-		icon_props: icon,
-		custom_icon,
-		expand_icon_position = 'right',
-		max_height_closed = 'calc(3ch + 0.5rem)',
-		max_height_open = 'calc(100% + 1rem)',
+		icon = default_icon,
+		icon_props,
+		icon_position = 'right',
 		open = $bindable(false),
-		open_icon_rotation = 90,
 		summary_attributes,
 		summary,
-		transition_props = {
-			slide_transition_parameters: { duration: 500, easing: 'ease' }
-		},
-		group_name,
+		show_icon = true,
+		on_toggle,
+		children,
 		...details_attributes
 	}: AccordionDetailsProps = $props();
 
-	const id = crypto.randomUUID();
-	let toggle: HTMLInputElement | undefined = $state();
-	let transition: TransitionNativeRunes | undefined = $state();
-
-	function toggle_accordion() {
-		if (open && toggle) {
-			toggle.checked = false;
-			open = false;
-		} else if (toggle) {
-			toggle.checked = true;
-			open = true;
-		}
-		transition?.toggle();
+	// Handle toggle
+	function handle_toggle() {
+		open = !open;
+		on_toggle?.(open);
 	}
-	let max_duration: number | undefined = $state(undefined);
-	$effect(() => {
-		max_duration =
-			transition_props?.slide_transition_parameters?.duration ??
-			transition_props?.blur_transition_parameters?.duration ??
-			transition_props?.fade_transition_parameters?.duration ??
-			transition_props?.scale_transition_parameters?.duration ??
-			transition_props?.fly_transition_parameters?.duration;
-	});
 </script>
-
-{#snippet fa_icon_snippet(icon_props: IconProps)}
-	<Icon {...icon_props} rotate={open ? open_icon_rotation : closed_icon_rotation} />
-{/snippet}
 
 {#snippet default_icon()}
 	<svg
-		style="tran"
-		style:rotate={open ? `${open_icon_rotation}deg` : `${closed_icon_rotation}deg`}
-		width="20"
-		height="20"
-		fill="none"
-		stroke-linecap="round"
-		stroke-linejoin="round"
-		stroke-width="2"
+		xmlns="http://www.w3.org/2000/svg"
+		aria-hidden="true"
+		role="img"
+		width="1em"
+		height="1em"
 		viewBox="0 0 24 24"
-		stroke="currentColor"><path d="M9 5l7 7-7 7" /></svg
 	>
+		<path fill="currentColor" d="M8.59 16.58L13.17 12L8.59 7.41L10 6l6 6l-6 6z" class:open />
+	</svg>
 {/snippet}
 
-<!--The `open` attribute on the <details> element is assigned to `true` to enable the transition trick with `max-height` in the CSS -->
-<!--svelte-ignore a11y_no_noninteractive_element_interactions-->
-<details
-	class={['_details', details_attributes.class]}
-	open={true}
-	style:--duration={max_duration ? `${max_duration}ms` : undefined}
-	style:--details-max-height-closed={max_height_closed}
-	style:--details-max-height-open={max_height_open}
-	onkeydown={(e) => {
-		if (e.key === 'Enter' || e.key === ' ') {
-			e.preventDefault();
-			toggle_accordion();
-		}
-	}}
-	{...details_attributes}
->
-	<summary onclick={toggle_accordion} {...summary_attributes}>
-		{#if group_name}
-			<input class:open bind:this={toggle} type="radio" name={group_name} {id} value={id} />
-		{:else}
-			<input class:open bind:this={toggle} type="checkbox" name={id} {id} bind:checked={open} />
-		{/if}
-
-		<label
-			onclick={toggle_accordion}
-			for={id}
-			onkeydown={(e) => {
-				if (e.key === 'Enter' || e.key === ' ') {
-					toggle_accordion();
-				}
-			}}
-		>
-			{#if expand_icon_position === 'left'}
-				{#if custom_icon}
-					{@render custom_icon()}
-				{:else if icon}
-					{@render fa_icon_snippet(icon)}
-				{:else}
-					{@render default_icon()}
-				{/if}
-			{/if}
-
-			{#if typeof summary !== 'string'}
-				{@render summary?.()}
+<details class={['_details', details_attributes.class]} {...details_attributes}>
+	<summary
+		onclick={handle_toggle}
+		{...summary_attributes}
+		class={[
+			{ show_icon, right: icon_position === 'right', left: icon_position === 'left' },
+			summary_attributes?.class
+		]}
+	>
+		<span style="grid-area: icon;">
+			{#if typeof icon === 'string'}
+				<Icon {icon} {...icon_props} />
 			{:else}
-				{summary}
+				{@render icon?.()}
 			{/if}
+		</span>
 
-			{#if expand_icon_position === 'right'}
-				{#if custom_icon}
-					{@render custom_icon()}
-				{:else if icon}
-					{@render fa_icon_snippet(icon)}
-				{:else}
-					{@render default_icon()}
-				{/if}
+		<div style="grid-area: summary;">
+			{#if typeof summary === 'string'}
+				<span class="summary-text">{summary}</span>
+			{:else}
+				{@render summary?.()}
 			{/if}
-		</label>
+		</div>
 	</summary>
-	<TransitionNativeRunes bind:this={transition} {...transition_props}>
-		{@render children?.()}
-	</TransitionNativeRunes>
+	<hr />
+	{@render children?.()}
 </details>
 
 <style>
@@ -185,20 +110,35 @@
 			cursor: pointer;
 		}
 		._details {
-			max-height: var(--details-max-height-closed, 1.5rem);
+			--duration: 500ms;
+			--easing: ease;
+			--details-max-height-closed: calc(3ch + 0.5rem);
+			--details-max-height-open: max-content;
 			overflow: hidden;
-			transition: max-height var(--duration, 400ms);
+			transition-property: box-shadow, border-radius;
+			transition-duration: var(--duration, 500ms);
 			transition-timing-function: var(
 				--details-transition-timing-function,
 				cubic-bezier(0.33, 0.68, 1)
 			);
-			border-radius: var(--accordion-button-border-radius, 1rem);
-			background-color: var(--details-background-color);
+			border-radius: var(--details-border-radius, var(--border-radius, revert));
+			background-color: var(--details-background-color, var(--background, inherit));
 			padding: var(--details-padding, 0.25rem 0.5rem);
-			margin: -0.5rem;
-			cursor: pointer;
-			/* overflow: auto; */
+
+			&::details-content {
+				content-visibility: auto;
+				transition-property: block-size, content-visibility;
+				transition-duration: var(--duration);
+				transition-timing-function: var(--easing);
+				transition-behavior: allow-discrete;
+				block-size: 0;
+			}
+			&[open]::details-content {
+				block-size: auto;
+				block-size: calc-size(auto, size);
+			}
 			&:hover {
+				box-shadow: var(--color-shadow-hover) 0 0 0.5rem;
 				outline: var(
 					--details-hover-outline,
 					ridge
@@ -208,6 +148,7 @@
 						)
 				);
 			}
+
 			&:focus-within {
 				outline: var(--details-focus-outline, ridge);
 			}
@@ -233,28 +174,39 @@
 			& > summary {
 				cursor: pointer;
 				list-style: none;
+				display: grid;
+				gap: 0.5rem;
+				align-items: center;
+				justify-items: start;
+				place-content: start;
+				&.show_icon.left {
+					grid-template-columns: auto 1fr;
+					grid-template-areas: 'icon summary';
+				}
+				&.show_icon.right {
+					grid-template-columns: 1fr auto;
+					grid-template-areas: 'summary icon';
+				}
+
 				&:focus-visible {
 					outline: none;
 				}
 				&::-webkit-details-marker {
 					display: none;
 				}
-				/* Customize the summary styles */
-				& > label {
-					display: flex;
-					justify-content: space-between;
-				}
-				/* Hide the checkbox */
-				& > input[type='checkbox'],
-				& > input[type='radio'] {
-					display: none;
-					pointer-events: none;
-				}
 			}
 		}
 
-		svg {
-			transition: rotate 0.2s ease-in;
+		path {
+			transition: rotate var(--duration) var(--easing);
+			transform-origin: center;
+			&.open {
+				rotate: 90deg;
+			}
+		}
+		hr {
+			margin: 1rem 0;
+			color: var(--border-color, inherit);
 		}
 	}
 </style>
--- src/routes/wrappers/accordion/+page.svelte
+++ src/routes/wrappers/accordion/+page.svelte
@@ -1,13 +1,13 @@
 <script lang="ts">
+	import { tooltip } from '$lib';
 	import Accordion from '$wrappers/Accordion.svelte';
 	import AccordionDetails, { type AccordionDetailsProps } from '$wrappers/AccordionDetails.svelte';
 	import AccordionJson from '$wrappers/AccordionJSON.svelte';
 	import JsonView from '$wrappers/JSONView.svelte';
-	import { tooltip } from '$lib';
 	import Treecordion from './Treecordion.svelte';
 
-	const details_styles = 'max-width: max-content;';
-	const summary_styles = 'min-width: min(20rem, 30vw)';
+	// const details_styles = 'max-width: max-content;';
+	// const summary_styles = 'min-width: min(20rem, 30vw)';
 
 	const value = {
 		things: ['one', 2, 'three', 'https://svelte.dev'],
@@ -22,71 +22,17 @@
 	);
 
 	const accordion_configs: AccordionDetailsProps[] = [
-		{
-			summary: opacity_default
-		},
-		{
-			summary: slide_snippet,
-			group_name: 'group_one',
-			transition_props: {
-				types: ['slide'],
-				slide_transition_parameters: { easing: 'ease' }
-			}
-		},
-		{
-			summary: scale_snippet,
-			group_name: 'group_one',
-			transition_props: {
-				types: ['scale'],
-				symmetrical: false,
-				scale_transition_parameters: { initial: 2, out_scale: 0 }
-			}
-		},
-		{
-			summary: blur_snippet,
-			group_name: 'group_one',
-			transition_props: { types: ['blur'] }
-		},
-		{
-			summary: fly_snippet,
-			group_name: 'group_one',
-			transition_props: { types: ['fly'], symmetrical: false }
-		},
-		{
-			summary: combo_snippet,
-			group_name: 'group_one',
-			transition_props: {
-				types: ['scale', 'blur'],
-				origin: 'top left',
-				blur_transition_parameters: { duration: 500, amount: 5 },
-				symmetrical: false,
-				scale_transition_parameters: { duration: 300, initial: 0, out_scale: 3 }
-			}
-		}
+		{ summary: 'A minimalistic opacity transition.', icon_position: 'left' },
+		{ summary: 'A slide transition.', icon_position: 'right' },
+		{ summary: 'A scale transition.', icon_position: 'left' },
+		{ summary: 'A blur transition.', icon_position: 'right' },
+		{ summary: 'A fly transition.', icon_position: 'left' },
+		{ summary: 'A combo transition.', icon_position: 'right' }
 	];
 	const content_text = `Hello from inside the accordion! Lorem ipsum dolor sit amet, consectetur adipisicing elit.
 			Odio repellat veritatis accusamus odit quod distinctio. Adipisci laudantium illum aliquam
 			omnis quae dolorum id accusantium assumenda! Nisi ullam aperiam inventore sint.`;
 </script>
-
-{#snippet opacity_default()}
-	A minimalistic opacity transition.
-{/snippet}
-{#snippet slide_snippet()}
-	A slide transition.
-{/snippet}
-{#snippet scale_snippet()}
-	An asymmetrical scaling transition.
-{/snippet}
-{#snippet blur_snippet()}
-	A blur transition.
-{/snippet}
-{#snippet fly_snippet()}
-	A asymmetrical fly-in transition.
-{/snippet}
-{#snippet combo_snippet()}
-	A combo transition.
-{/snippet}
 
 {#snippet content()}
 	<div class="content">
@@ -98,8 +44,8 @@
 	{#each accordion_configs as accordion, i}
 		<AccordionDetails
 			{...accordion}
-			style={details_styles}
-			summary_attributes={{ style: summary_styles }}
+			name="group_one"
+			style="border-radius: 1rem;"
 			{@attach tooltip({ content: 'A summary tooltip' })}
 		>
 			{@render content()}
