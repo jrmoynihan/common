e2fc53d6e9017b7dbc6597b10ef4c910b26470ae
jrmoynihan <jrmoynihan@gmail.com>
2026-03-16 09:23:07 -0400

    refactor(Placeholder): :sparkles: Improve placeholder styling and class handling
    Updated the Placeholder component to enhance class management and styling consistency. Introduced a new class structure for better layout control and applied styles within a dedicated layer for improved organization.

--- src/lib/inputs/Placeholder.svelte
+++ src/lib/inputs/Placeholder.svelte
@@ -16,32 +16,30 @@
 	role="presentation"
 	inert
 	bind:this={placeholder_element}
-	class={['placeholder', attributes?.class]}
 	{...attributes}
+	class={[
+		'_placeholder box-border grid overflow-auto text-ellipsis whitespace-nowrap transition-all duration-300 ease-in-out',
+		attributes?.class
+	]}
 >
 	{text ?? ''}
 </div>
 
 <style>
-	.placeholder {
-		--text: light-dark(dimgray, lightgray);
-		user-select: none;
-		font-size: var(--placeholder-font-size, 1em);
-		font-style: var(--placeholder-font-style, normal);
-		font-family: var(--placeholder-font-family, inherit);
-		inset: 0;
-		transition: all ease-in-out 300ms;
-		pointer-events: none;
-		text-overflow: ellipsis;
-		white-space: nowrap;
-		overflow-x: auto;
-		overflow-y: auto;
-		display: grid;
-		box-sizing: border-box;
-		grid-area: input;
-		padding: var(--text-input-padding, 1.25em);
-		color: var(--placeholder-color, oklch(from var(--text) l c h / 0.7));
-		z-index: 0; /* addresses stacking context issue on the same grid-area */
-		/* height: 100%;  /* causes layout bug in Safari */
+	@layer common.input.placeholder {
+		._placeholder {
+			--text: light-dark(dimgray, lightgray);
+			user-select: none;
+			font-size: var(--placeholder-font-size, 1em);
+			font-style: var(--placeholder-font-style, normal);
+			font-family: var(--placeholder-font-family, inherit);
+			inset: 0;
+			pointer-events: none;
+			grid-area: input;
+			padding: var(--text-input-padding, 1.25em);
+			color: var(--placeholder-color, oklch(from var(--text) l c h / 0.7));
+			z-index: 0; /* addresses stacking context issue on the same grid-area */
+			/* height: 100%;  /* causes layout bug in Safari */
+		}
 	}
 </style>
